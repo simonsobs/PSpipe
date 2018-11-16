cfc71ede3c4c25bdf1ae77fa584554ed9ebcd6a8
dwhan89 <retimes@hotmail.com>
2018-11-16 04:23:00 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#- 
+# setup.py
+#-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'PS PY',
+    'author':  "Steve Choi, Thibaut Louis, Dongwon 'DW' HAN",
+    'url': 'https://github.com/simonsobs/ps_py',
+    'download_url': 'https://github.com/simonsobs/ps_py',
+    'version': '0.0.1',
+    'install_requires': [
+        'numpy',
+        'pixell'
+        ],
+    'packages': [
+        'pspy',
+        ],
+    'scripts': [],
+    'name': 'pspy'
+}
+
+setup(**config)
+

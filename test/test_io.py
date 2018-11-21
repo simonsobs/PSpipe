5d05d9c17b5a2986208ea4f4b4aa5179db68b735
Thibaut <thibaut@thibauts-macbook-pro.home>
2018-11-21 16:50:09 +0100

    add comment and remove trash

--- test/test_io.py
+++ test/test_io.py
@@ -15,37 +15,37 @@ res=1
 ncomp=3
 clfile='../data/bode_almost_wmap5_lmax_1e4_lensedCls.dat'
 
-#We generate both a CAR and HEALPIX template
+#We generate both a CAR and HEALPIX templates
 
 template_car= so_map.car_template(ncomp,ra0,ra1,dec0,dec1,res)
 template_healpix= so_map.healpix_template(ncomp,nside=256,coordinate='equ')
 
-#We feel the template with CMB data
+#We fill the templates with CMB data
 
 cmb_car=template_car.synfast(clfile)
 cmb_healpix=template_healpix.synfast(clfile)
 
-#We plot it
+#We plot them
 
 cmb_car.plot(file_name='map_car_io_test')
 cmb_healpix.plot(file_name='map_healpix_io_test')
 
-#We write it to disk
+#We write them to disk
 
 cmb_car.write_map('map_car.fits')
 cmb_healpix.write_map('map_healpix.fits')
 
-#We read the map
+#We read the maps
 
 cmb_car2=so_map.read_map('map_car.fits')
 cmb_healpix2=so_map.read_map('map_healpix.fits')
 
-#We null it
+#We null them
 
 cmb_car2.data-=cmb_car.data
 cmb_healpix2.data-=cmb_healpix.data
 
-#We plot the null: note that while car is completely zero, some low amplitude numerical noise is there in healpix
+#We plot the nulls: note that while car is  zero, some low amplitude numerical noise is there in healpix
 
 cmb_car2.plot(file_name='map_car_null')
 cmb_healpix2.plot(file_name='map_healpix_null')

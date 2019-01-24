c24a207508bdf5fad741bc504f5414c03f5bd7e0
dwhan89 <retimes@hotmail.com>
2019-01-24 04:47:53 -0800

    updated mc cov test script

--- test/test_mc_cov.py
+++ test/test_mc_cov.py
@@ -7,6 +7,7 @@
 from pspy import so_map, so_window, pspy_utils, so_mpi, so_mcm, so_config, so_spectra, sph_tools
 from pixell import curvedsky, utils, enmap
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 sns.set()
@@ -17,7 +18,7 @@ output_dir     = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_
 mcm_prefix     = os.path.join(output_dir, "mcm") 
 output_path    = lambda x: os.path.join(output_dir, x) 
 alm_input_dir  = '/global/cscratch1/sd/engelen/simsS1516_v0.4/data'
-alm_input_temp = os.path.join(alm_input_dir, 'fullskyLensedUnabberatedCMB_alm_set00_{0:05}.fits') 
+alm_input_temp = os.path.join(alm_input_dir, 'fullskyLensedUnabberatedCMB_alm_set01_{0:05}.fits') 
 
 if so_mpi.rank == 0:
     pspy_utils.create_directory(output_dir)
@@ -38,6 +39,17 @@ theory_file          = os.path.join(so_config.DEFAULT_DATA_DIR, 'cosmo2017_10K_a
 window   = so_map.car_template(1, ra0, ra1, dec0, dec1, res)
 window   = so_window.create_apodization(window, 'Rectangle', 2)
 window.plot(file_name=output_path('window'))
+
+# helper function
+def stats(data, axis=0, ddof=1.):
+    datasize = data.shape
+    mean     = np.mean(data, axis=axis)
+    cov      = np.cov(data.transpose(), ddof=ddof)
+    corrcoef = np.corrcoef(data.transpose())
+    std      = np.std(data, axis=axis, ddof=ddof) # use the N-1 normalization
+    std_mean = std/np.sqrt(datasize[0])
+
+    return {'mean': mean, 'cov': cov, 'corrcoef': corrcoef, 'std': std, 'datasize': datasize, 'std_mean': std_mean}
 
 # create mcm
 if so_mpi.rank == 0 and not os.path.exists('{}_Bbl_spin0xspin0.npy'.format(mcm_prefix)):
@@ -81,4 +93,57 @@ for sim_idx in subtasks:
     ls, ps = so_spectra.get_spectra(alms, alms, spectra=spectra)
     lb, Db = so_spectra.bin_spectra(ls, ps, binning_file, type='Dl', lmax=lmax, mbb_inv=mbb_inv,spectra=spectra)
     so_spectra.write_ps(output_path('spectra_grf_ncomp%d_%04d.dat'%(ncomp,sim_idx)),lb,Db,'Dl',spectra=spectra)
+
+if so_mpi.rank == 0:
+    Db_dict    = {}
+    st         = {}
+    data_types = ['lensed', 'grf']
+    sim_idxes  = range(0, nsims) 
+    for data_type in data_types:
+        Db_dict[data_type] = {}
+        for spec in spectra:
+            Db_dict[data_type][spec] ={}
+        for sim_idx in sim_idxes:
+            lb,Db=so_spectra.read_ps(output_path('spectra_%s_ncomp%d_%04d.dat'%(data_type,ncomp,sim_idx)),spectra=spectra)
+            if not Db_dict[data_type].has_key('lbin'): Db_dict[data_type]['lbin'] = lb
+            for spec in spectra:
+                Db_dict[data_type][spec][sim_idx] = Db[spec]
+
+        st[data_type] = {}
+        nspec    = len(spectra)
+        nbin     = len(Db_dict[data_type]['lbin'])
+        data = np.zeros((nsims, nbin*nspec))
+        ctr  = 0
+        for ctr, spec in enumerate(spectra):
+            data[:,ctr*nbin:(ctr+1)*nbin] = np.array(Db_dict[data_type][spectra[ctr]].values()).copy()
+            st[data_type][spec]     = stats(np.array(Db_dict[data_type][spectra[ctr]].values()).copy())
+        st[data_type]['full'] = stats(data)
+    
+    # plot spectra
+    plt.figure(figsize=(20,15))
+    for data_type in data_types:
+        for c,spec in enumerate(spectra):
+            mean=st[data_type][spec]['mean']
+            std =st[data_type][spec]['std_mean']
+            plt.subplot(3,3,c+1)
+            plt.errorbar(lb,mean,std,fmt='o',label='%s'%data_type)
+            plt.ylabel(r'$D^{%s}_{\ell}$'%spec,fontsize=20)
+            plt.xlabel(r'$\ell$',fontsize=20)
+            if c==0:
+                plt.legend()
+    plt.savefig(output_path('spectra.png'),bbox_inches='tight')
+    plt.clf()
+    plt.close()
+
+    for data_type in data_types:
+        for spec in spectra:
+            std  = np.sqrt(np.diag(st[data_type][spec]['cov']))
+            corr = st[data_type][spec]['cov']/np.outer(std,std)
+            np.fill_diagonal(corr, 0.)
+            ax = sns.heatmap(corr, xticklabels=10, yticklabels=10)
+            plt.title('%s%s correlation matrix (%s)' %(spec.upper(), spec.upper(), data_type.upper()))
+            plt.savefig(output_path('correlation_%s_%s.png'%(data_type, spec)), bbox_inches='tight')
+            plt.clf()
+            plt.close()
+ 
 

028adcac77c4aa3d4a7b46dc42704f91e7e2efe2
Thibaut <thibaut@adapt-Louis.lal.in2p3.fr>
2019-03-28 10:42:00 +0100

    add planck spectra from erminia files

--- project/Planck/plot_spectra.py
+++ project/Planck/plot_spectra.py
@@ -8,7 +8,7 @@ name=['TT_100x100','TT_143x143','TT_143x217','TT_217x217']
 plt.semilogy()
 for n in name:
     print (n)
-    l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
+    lmin,lmax,l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
     plt.errorbar(l,cl*l**2/(2*np.pi),error*l**2/(2*np.pi),fmt='.',label='%s'%n)
 plt.legend()
 plt.show()
@@ -16,7 +16,7 @@ plt.show()
 name=['EE_100x100','EE_100x143','EE_100x217','EE_143x143','EE_143x217','EE_217x217']
 for n in name:
     print (n)
-    l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
+    lmin,lmax,l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
     plt.errorbar(l,cl*l**2/(2*np.pi),error*l**2/(2*np.pi),fmt='.',label='%s'%n)
 plt.legend()
 plt.show()
@@ -24,7 +24,7 @@ plt.show()
 name=['TE_100x100','TE_100x143','TE_100x217','TE_143x143','TE_143x217','TE_217x217']
 for n in name:
     print (n)
-    l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
+    lmin,lmax,l,cl,error=np.loadtxt('Planck_data/spectra_' + n + '.dat',unpack=True)
     plt.errorbar(l,cl*l**2/(2*np.pi),error*l**2/(2*np.pi),fmt='.',label='%s'%n)
 plt.legend()
 plt.show()

e3a855a0c5c49a75bb376f264d549a4a22fdf710
Thibaut <thbtlouis@gmail.com>
2019-05-15 12:10:55 +0200

    attempt to remove monopole/dipole for the spectra deconvolution

--- pspy/so_spectra.py
+++ pspy/so_spectra.py
@@ -94,7 +94,7 @@ def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=No
             mcm_inv=so_mcm.coupling_dict_to_array(mcm_inv)
             for f in spectra:
                 unbin_vec=np.append(unbin_vec,cl[f][2:])
-            cl=vec2spec_dict(lmax-2,np.dot(mcm_inv[:-2,:-2],unbin_vec),spectra)
+            cl=vec2spec_dict(lmax-1,np.dot(mcm_inv[:-1,:-1],unbin_vec),spectra)
 
         vec=[]
         for f in spectra:

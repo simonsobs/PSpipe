b22fd0243f2c9875b468807e9e1a89dff9e15900
Thibaut <thbtlouis@gmail.com>
2019-06-04 11:18:35 +0200

    script to plot the sims

--- project/AnalyseSims/plot_sims.py
+++ project/AnalyseSims/plot_sims.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
-from __future__ import print_function
+import matplotlib
+matplotlib.use('Agg')
 from pspy import so_map,so_window,so_mcm,sph_tools,so_spectra, pspy_utils, so_dict
 import healpy as hp, numpy as np, pylab as plt
 import os,sys

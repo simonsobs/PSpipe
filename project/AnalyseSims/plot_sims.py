8283a2f1606a2f2564e03c74c29beb46db24faa4
Thibaut <thbtlouis@gmail.com>
2019-06-04 12:20:58 +0200

    plot sim

--- project/AnalyseSims/plot_sims.py
+++ project/AnalyseSims/plot_sims.py
@@ -27,11 +27,11 @@ for exp in experiment:
         maps_list= d['%s_maps'%cont]
         for map,f in zip(maps_list,freqs):
             map=so_map.read_map(map)
-            map.plot(file_name='%s/%s_%s_%s.png'%(plot_dir,cont,exp,f))
+            map.plot(file_name='%s/%s_%s_%s'%(plot_dir,cont,exp,f),color_range=(250,50,50))
 
     masks= d['masks']
     for mask,f in zip(masks,freqs):
         mask=so_map.read_map(mask)
-        mask.plot(file_name='%s/mask_%s_%s.png'%(plot_dir,exp,f))
+        mask.plot(file_name='%s/mask_%s_%s'%(plot_dir,exp,f))
 
 

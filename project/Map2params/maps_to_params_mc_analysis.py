cc808bb45778aef855ba0471e4c9b136fc9fb403
Thibaut <thbtlouis@gmail.com>
2019-06-06 17:08:10 +0200

    add foregrounds to map2params sim

--- project/Map2params/maps_to_params_mc_analysis.py
+++ project/Map2params/maps_to_params_mc_analysis.py
@@ -1,4 +1,5 @@
 from pspy import pspy_utils, so_dict,so_map,so_mpi,sph_tools,so_mcm,so_spectra
+from matplotlib.pyplot import cm
 import  numpy as np, pylab as plt, healpy as hp
 import os,sys
 import so_noise_calculator_public_20180822 as noise_calc
@@ -21,6 +22,9 @@ lcut=d['lcut']
 hdf5=d['hdf5']
 multistep_path=d['multistep_path']
 
+foreground_dir=d['foreground_dir']
+extragal_foregrounds=d['extragal_foregrounds']
+
 specDir='spectra'
 
 if hdf5:
@@ -28,6 +32,8 @@ if hdf5:
 
 mcm_dir='mcm'
 plot_dir='plot'
+
+
 pspy_utils.create_directory(plot_dir)
 
 ncomp=3
@@ -64,27 +70,36 @@ for id_exp1,exp1 in enumerate(experiment):
                     for spec in spectra:
                         nlth[spec]=np.zeros(lmax)
 
-
                 prefix= '%s/%s_%sx%s_%s'%(mcm_dir,exp1,f1,exp2,f2)
                 mbb_inv,Bbl=so_mcm.read_coupling(prefix=prefix,spin_pairs=spin_pairs)
 
                 for kind in ['cross','noise','auto']:
-
                     ps_th={}
                     for spec in spectra:
+                        
+                        ps=Dlth[spec].copy()
+                        
+                        if spec=='TT':
+                            flth_all=0
+                            for foreground in extragal_foregrounds:
+                                l,flth=np.loadtxt('%s/%s_%sx%s.dat'%(foreground_dir,foreground,f1,f2),unpack=True)
+                                flth_all+=flth[:lmax]
+                            ps=Dlth[spec]+flth_all
+                    
                         if kind=='cross':
-                            ps_th[spec]=Dlth[spec]
+                            ps_th[spec]=ps
                         elif kind=='noise':
                             ps_th[spec]=nlth[spec]*lth**2/(2*np.pi)
                         elif kind=='auto':
-                            ps_th[spec]=Dlth[spec]+nlth[spec]*lth**2/(2*np.pi)*ns[exp1]
+                            ps_th[spec]=ps+nlth[spec]*lth**2/(2*np.pi)*ns[exp1]
     
                     theory[exp1,f1,exp2,f2,kind]=ps_th
                     bin_theory[exp1,f1,exp2,f2,kind]=so_mcm.apply_Bbl(Bbl,ps_th,spectra=spectra)
 
-
-
 spec_name={}
+mean_dict={}
+std_dict={}
+n_spec={}
 for kind in ['cross','noise','auto']:
     vec_list=[]
     for iii in range(iStart,iStop):
@@ -101,7 +116,6 @@ for kind in ['cross','noise','auto']:
                             if  (id_exp1>id_exp2) : continue
                             if (exp1!=exp2) & (kind=='noise'): continue
                             if (exp1!=exp2) & (kind=='auto'): continue
-
 
                             spec_name='%s_%s_%sx%s_%s_%s_%05d'%(type,exp1,f1,exp2,f2,kind,iii)
                     
@@ -122,6 +136,9 @@ for kind in ['cross','noise','auto']:
 
     id_spec=0
     for spec in spectra:
+        
+        n_spec[kind]=0
+
         for id_exp1,exp1 in enumerate(experiment):
             freqs1=d['freq_%s'%exp1]
             for id_f1,f1 in enumerate(freqs1):
@@ -134,9 +151,12 @@ for kind in ['cross','noise','auto']:
                         if (exp1!=exp2) & (kind=='auto'): continue
 
                 
-                        mean=mean_vec[id_spec*n_bins:(id_spec+1)*n_bins]
-                        std=np.sqrt(cov[id_spec*n_bins:(id_spec+1)*n_bins,id_spec*n_bins:(id_spec+1)*n_bins].diagonal())
+                        mean_dict[kind,spec,exp1,f1,exp2,f2]=mean_vec[id_spec*n_bins:(id_spec+1)*n_bins]
+                        std_dict[kind,spec,exp1,f1,exp2,f2]=np.sqrt(cov[id_spec*n_bins:(id_spec+1)*n_bins,id_spec*n_bins:(id_spec+1)*n_bins].diagonal())
                 
+                        mean=mean_dict[kind,spec,exp1,f1,exp2,f2]
+                        std=std_dict[kind,spec,exp1,f1,exp2,f2]
+
                         plt.figure(figsize=(8,7))
                 
                         if spec=='TT':
@@ -167,8 +187,33 @@ for kind in ['cross','noise','auto']:
                         plt.clf()
                         plt.close()
 
-
                         id_spec+=1
+                        n_spec[kind]+=1
+
+
+plt.figure(figsize=(12,12))
+color=iter(cm.rainbow(np.linspace(0,1,n_spec['cross']+1)))
+
+
+for id_exp1,exp1 in enumerate(experiment):
+    freqs1=d['freq_%s'%exp1]
+    for id_f1,f1 in enumerate(freqs1):
+        for id_exp2,exp2 in enumerate(experiment):
+            freqs2=d['freq_%s'%exp2]
+            for id_f2,f2 in enumerate(freqs2):
+                if  (id_exp1==id_exp2) & (id_f1>id_f2) : continue
+                if  (id_exp1>id_exp2) : continue
+
+                c=next(color)
+                plt.errorbar(lb,mean_dict['cross','TT',exp1,f1,exp2,f2],std_dict['cross','TT',exp1,f1,exp2,f2],fmt='.',color=c,label='%s%s x %s%s'%(exp1,f1,exp2,f2))
+                plt.plot(lth,theory[exp1,f1,exp2,f2,'cross']['TT'],color=c,alpha=0.4)
+
+plt.legend()
+plt.xlabel(r'$\ell$',fontsize=20)
+plt.savefig('plot/all_spectra_TT_%s_%sx%s_%s_cross.png'%(exp1,f1,exp2,f2),bbox_inches='tight')
+plt.clf()
+plt.close()
+
 
 
 os.system('cp %s/multistep2.js %s/multistep2.js'%(multistep_path,plot_dir))

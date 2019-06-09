acbd399b53dfd309faf9e0ae347bc9afd0130d46
Thibaut <thbtlouis@gmail.com>
2019-06-09 10:50:32 +0200

    add spectra utilities

--- project/AnalyseSims/combine_sims.py
+++ project/AnalyseSims/combine_sims.py
@@ -11,7 +11,7 @@ d.read_from_file(sys.argv[1])
 content=d['content']
 
 plot_dir='plot'
-combined_map_dir='combined_map'
+combined_map_dir='combined_maps'
 
 pspy_utils.create_directory(plot_dir)
 pspy_utils.create_directory(combined_map_dir)
@@ -47,23 +47,22 @@ for exp in experiment:
         noise_map0.data/=np.sqrt(2)
         noise_map1.data/=np.sqrt(2)
 
-        survey_mask_list= d['survey_masks']
-        survey_mask=so_map.read_map(survey_mask_list[count])
+        survey_mask=so_map.read_map(d['survey_mask_%s_%s'%(exp,freq)])
         
         color_range=(200,20,20)
 
         map_all.data*=survey_mask.data
         map_all.plot(file_name='%s/combined_%s_%s'%(plot_dir,exp,freq),color_range=color_range)
-        map_all.write_map('%s/combined_map_%s.fits'%(combined_map_dir,freq))
+        map_all.write_map('%s/combined_map_%s_%s.fits'%(combined_map_dir,exp,freq))
         
         color_range=(400,40,40)
 
         noise_map0.data+=map_all.data
         noise_map0.data*=survey_mask.data
         noise_map0.plot(file_name='%s/combined_%s_%s_noise0'%(plot_dir,exp,freq),color_range=color_range)
-        noise_map0.write_map('%s/combined_map_%s_noise0.fits'%(combined_map_dir,freq))
+        noise_map0.write_map('%s/combined_map_%s_%s_noise0.fits'%(combined_map_dir,exp,freq))
         
         noise_map1.data+=map_all.data
         noise_map1.data*=survey_mask.data
         noise_map1.plot(file_name='%s/combined_%s_%s_noise1'%(plot_dir,exp,freq),color_range=color_range)
-        noise_map1.write_map('%s/combined_map_%s_noise1.fits'%(combined_map_dir,freq))
+        noise_map1.write_map('%s/combined_map_%s_%s_noise1.fits'%(combined_map_dir,exp,freq))

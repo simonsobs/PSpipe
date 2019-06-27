6b55e42c076a30ddbf1c74b66a9de753d7dcd427
Thibaut <thbtlouis@gmail.com>
2019-06-27 13:51:13 +0200

    add script to compute the covariance matrix

--- project/AnalyseSims/sim_covariance.py
+++ project/AnalyseSims/sim_covariance.py
@@ -1,7 +1,6 @@
 from pspy import pspy_utils, so_dict,so_map,so_mpi,sph_tools,so_mcm,so_spectra,so_cov
 import  numpy as np, pylab as plt, healpy as hp
 import os,sys
-import so_noise_calculator_public_20180822 as noise_calc
 from pixell import curvedsky,powspec
 import maps_to_params_utils
 import h5py

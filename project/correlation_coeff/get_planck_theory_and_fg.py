99b385731046ee1bd25ed83d6143fd8abfe3a4be
Thibaut <thbtlouis@gmail.com>
2019-08-31 14:29:09 +0200

    last version

--- project/correlation_coeff/get_planck_theory_and_fg.py
+++ project/correlation_coeff/get_planck_theory_and_fg.py
@@ -14,8 +14,6 @@ theoryFgDir='theory_and_fg'
 
 pspy_utils.create_directory(theoryFgDir)
 freqs=d['freqs']
-include_foregrounds=d['include_foregrounds']
-
 spectra=['TT','TE','TB','ET','BT','EE','EB','BE','BB']
 
 
@@ -36,8 +34,8 @@ clth['BB']=cl_BB
 
 lth,fg['TT','100x100'],fg['TT','143x143'],fg['TT','143x217'],fg['TT','217x217'],fg['EE','100x100'],fg['EE','100x143'],fg['EE','100x217'],fg['EE','143x143'],fg['EE','143x217'],fg['EE','217x217'],fg['TE','100x100'],fg['TE','100x143'],fg['TE','100x217'],fg['TE','143x143'],fg['TE','143x217'],fg['TE','217x217']=np.loadtxt('theory_file/base_plikHM_TTTEEE_lowl_lowE_lensing.minimum.plik_foregrounds',unpack=True)
 
-fg['TT','100x143']=np.sqrt(fg['TT','100x100']*fg['TT','143x143'])
-fg['TT','100x217']=np.sqrt(fg['TT','100x100']*fg['TT','217x217'])
+fg['TT','100x143']=fg['TT','100x100']*0
+fg['TT','100x217']=fg['TT','100x100']*0
 
 # The foreground+syst spectra have a very strange shape at high ell, we therefore regularize them
 # Note that the scales beyond the regularisation scale will not be used in the paper
@@ -46,8 +44,9 @@ lth_max=6000
 l_regul=1450
 fg_regularised=np.zeros(lth_max)
 lth=np.arange(2,lth_max+2)
-lth_padded=np.arange(lth_max)
+lth_padded=np.arange(0,lth_max)
 fth=lth*(lth+1)/(2*np.pi)
+fth_padded=lth_padded*(lth_padded+1)/(2*np.pi)
 cl_th_and_fg={}
 
 for spec in ['TT','EE','TE']:
@@ -60,16 +59,10 @@ for spec in ['TT','EE','TE']:
         fg_regularised[l_regul:]=fg_regularised[l_regul-1]
         
         cl_th_and_fg[spec,fname]=np.zeros(lth_max)
-        
-        if include_foregrounds:
-            cl_th_and_fg[spec,fname][2:lth_max]=clth[spec][:lth_max-2]/fth[:lth_max-2]+fg_regularised[:lth_max-2]
-        else:
-            cl_th_and_fg[spec,fname][2:lth_max]=clth[spec][:lth_max-2]/fth[:lth_max-2]
-
-        
+        cl_th_and_fg[spec,fname][2:lth_max]=clth[spec][:lth_max-2]/fth[:lth_max-2]+fg_regularised[:lth_max-2]
         np.savetxt('%s/clth_fg_%s_%s.dat'%(theoryFgDir,fname,spec),np.transpose( [lth_padded,cl_th_and_fg[spec,fname]]))
         
-        plt.plot(lth_padded,cl_th_and_fg[spec,fname]*fth,label='%s'%fname)
+        plt.plot(lth_padded,cl_th_and_fg[spec,fname]*fth_padded,label='%s'%fname)
     plt.legend()
     plt.savefig('%s/clth_fg_%s.png'%(theoryFgDir,spec))
     plt.clf()
@@ -102,4 +95,6 @@ for c1,freq1 in enumerate(freqs):
 
 
 ps_th=powspec.read_spectrum(d['theoryfile'])[:3,:3]
+
+
 np.save('%s/signal_fg_matrix.npy'%theoryFgDir,mat)

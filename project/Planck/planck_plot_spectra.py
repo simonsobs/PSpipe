b35382517845e1a068bb44aa8a0904c6947a2471
Thibaut <thbtlouis@gmail.com>
2020-01-23 15:32:15 +0100

    add freq paris

--- project/Planck/planck_plot_spectra.py
+++ project/Planck/planck_plot_spectra.py
@@ -19,44 +19,49 @@ pol_efficiency["100"] = 0.9995
 pol_efficiency["143"] = 0.999
 pol_efficiency["217"] = 0.999
 
-for freq in freqs:
-    spec_name = "Planck_%sxPlanck_%s-%sx%s" % (freq, freq, "hm1", "hm2")
-    file_name = "%s/spectra_%s.dat" % (spectra_dir, spec_name)
-    lb, ps_dict = so_spectra.read_ps(file_name, spectra=spectra)
-    
-    for spec in ["TT", "TE", "EE"]:
-        planck_name = "%s_%sx%s" % (spec, freq, freq)
-        l, cl, error = np.loadtxt("data/planck_data/spectrum_" + planck_name + ".dat", unpack=True)
-        id = np.where((lb >= l[0]) & (lb <= l[-1]))
-        lb_sel = lb.copy()
-        lb_sel = lb_sel[id]
+for f1, freq1 in enumerate(freqs):
+    for f2, freq2 in enumerate(freqs):
+        if f1 > f2: continue
 
-        if spec == "TE":
-            ps_dict["TE"] = (ps_dict["TE"] + ps_dict["ET"]) / 2
-            ps_dict["TE"] *= pol_efficiency[freq]
+        spec_name = "Planck_%sxPlanck_%s-%sx%s" % (freq1, freq2, "hm1", "hm2")
+        file_name = "%s/spectra_%s.dat" % (spectra_dir, spec_name)
+        lb, ps_dict = so_spectra.read_ps(file_name, spectra=spectra)
     
-        if spec == "EE":
-            ps_dict["EE"] *= pol_efficiency[freq]**2
+        for spec in ["TT", "TE", "EE"]:
+            planck_name = "%s_%sx%s" % (spec, freq1, freq2)
+            l, cl, error = np.loadtxt("data/planck_data/spectrum_" + planck_name + ".dat", unpack=True)
+            id = np.where((lb >= l[0]) & (lb <= l[-1]))
+            lb_sel = lb.copy()
+            lb_sel = lb_sel[id]
+
+            if spec == "TE":
+                ps_dict["TE"] = (ps_dict["TE"] + ps_dict["ET"]) / 2
+                ps_dict["TE"] *= pol_efficiency[freq]
+    
+            if spec == "EE":
+                ps_dict["EE"] *= pol_efficiency[freq]**2
         
-        ps_dict[spec] = ps_dict[spec][id]
+            ps_dict[spec] = ps_dict[spec][id]
+            
+            fpair = "%sx%s"%(freq1,freq2)
 
-        plt.figure(figsize=(14, 7))
-        plt.subplot(2, 1, 1)
-        plt.errorbar(lb_sel, ps_dict[spec]*lb_sel**2/(2*np.pi), label="planck pspy %s GHz" % freq, color="black")
-        plt.errorbar(l, cl*l**2/(2*np.pi), error*l**2/(2*np.pi), fmt=".", label="planck %s GHz" % freq, color="red")
-        plt.xlabel(r"$\ell$", fontsize=22)
-        plt.ylabel(r"$\ell^2 C_\ell/(2 \pi)$", fontsize=22)
-        plt.legend()
-        plt.subplot(2, 1, 2)
-        plt.plot(lb_sel, lb_sel*0, color="black")
-        plt.errorbar(lb_sel, (ps_dict[spec]-cl)/error, label="frac %s" % freq, color="red")
-        plt.xlabel(r"$\ell$", fontsize=22)
-        plt.ylabel(r"$\Delta C_\ell/\sigma_{\ell} $", fontsize=22)
-        plt.ylim(-0.5, 0.5)
-        plt.legend()
-        plt.savefig("%s/redo_planck_%s_%s.png"%(plot_dir,spec,freq))
-        plt.clf()
-        plt.close()
+            plt.figure(figsize=(14, 7))
+            plt.subplot(2, 1, 1)
+            plt.errorbar(lb_sel, ps_dict[spec]*lb_sel**2/(2*np.pi), label="planck pspy %s GHz" % fpair, color="black")
+            plt.errorbar(l, cl*l**2/(2*np.pi), error*l**2/(2*np.pi), fmt=".", label="planck %s GHz" % fpair, color="red")
+            plt.xlabel(r"$\ell$", fontsize=22)
+            plt.ylabel(r"$\ell^2 C_\ell/(2 \pi)$", fontsize=22)
+            plt.legend()
+            plt.subplot(2, 1, 2)
+            plt.plot(lb_sel, lb_sel*0, color="black")
+            plt.errorbar(lb_sel, (ps_dict[spec]-cl)/error, label="frac %s" % fpair, color="red")
+            plt.xlabel(r"$\ell$", fontsize=22)
+            plt.ylabel(r"$\Delta C_\ell/\sigma_{\ell} $", fontsize=22)
+            plt.ylim(-0.5, 0.5)
+            plt.legend()
+            plt.savefig("%s/redo_planck_%s_%s.png"%(plot_dir,spec,fpair))
+            plt.clf()
+            plt.close()
 
 
 

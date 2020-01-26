d5fd0f7c953098b7529e2eb19351918a3d6459ca
Thibaut <thbtlouis@gmail.com>
2020-01-26 09:06:32 +0100

    some cleaning

--- project/Planck_pspy/python/planck_sim_analysis.py
+++ project/Planck_pspy/python/planck_sim_analysis.py
@@ -22,6 +22,9 @@ lmax = d["lmax"]
 lrange = d["lrange"]
 bestfit_dir = "best_fits"
 
+
+plot_dir = "plots/sim_analysis/"
+
 if d["use_ffp10"] == True:
     sim_spectra_dir = "sim_spectra_ffp10"
     mc_dir = "montecarlo_ffp10"
@@ -29,18 +32,17 @@ else:
     sim_spectra_dir = "sim_spectra"
     mc_dir = "montecarlo"
 
-
 pspy_utils.create_directory(mc_dir)
+pspy_utils.create_directory(plot_dir)
 
 freq_pairs = []
 for cross in cwr(freqs, 2):
-    freq_pairs += [[cross[0],cross[1]]]
-
+    freq_pairs += [[cross[0], cross[1]]]
 
 vec_list = []
-binrange = {}
+bin_range = {}
 
-for iii in range(iStart,iStop):
+for iii in range(iStart, iStop):
     vec = []
     bin_start = 0
     bin_stop = 0
@@ -65,11 +67,9 @@ for iii in range(iStart,iStop):
                 if spec == "TE":
                     ps_dict["ET"] = (ps_dict["ET"] +  ps_dict2["ET"]) / 2
 
-
             if spec == "TE":
                 ps_dict["TE"] = (ps_dict["TE"] + ps_dict["ET"]) / 2
-            
-            
+        
             ps_name = "%s_%sx%s" % (spec, f0, f1)
 
             lmin_c, lmax_c = lrange[ps_name]
@@ -81,18 +81,20 @@ for iii in range(iStart,iStop):
             vec = np.append(vec, ps_dict[spec])
             
             bin_stop += len(ps_dict[spec])
-            binrange[ps_name] = bin_start, bin_stop
+            bin_range[ps_name] = bin_start, bin_stop
             bin_start = bin_stop
 
     vec_list += [vec]
 
 mean_vec = np.mean(vec_list, axis=0)
+
 cov = 0
 for iii in range(iStart, iStop):
     cov += np.outer(vec_list[iii], vec_list[iii])
-cov = cov / (iStop-iStart) - np.outer(mean_vec, mean_vec)
+cov = cov / (iStop - iStart) - np.outer(mean_vec, mean_vec)
 
 np.save("%s/mc_covariance.npy" % (mc_dir), cov)
+
 inv_cov_planck = np.load("data/planck_data/covmat.npy")
 cov_planck = np.linalg.inv(inv_cov_planck)
 variance_planck = cov_planck.diagonal()
@@ -110,30 +112,42 @@ for spec in ["TT", "EE", "TE"]:
         if (spec == "TT") & (f0 == "100") & (f1 == "143"): continue
         if (spec == "TT") & (f0 == "100") & (f1 == "217"): continue
         
-        lplanck, cl_planck, error_planck = np.loadtxt("data/planck_data/spectrum_%s_%sx%s.dat"%(spec,f0,f1), unpack=True)
-        id_high_planck += len(lplanck)
-        
-        ps_name = "%s_%sx%s" % (spec, f0, f1)
-        lmin_c, lmax_c = lrange[ps_name]
-        
+        l_planck, _, _ = np.loadtxt("data/planck_data/spectrum_%s_%sx%s.dat"%(spec, f0, f1), unpack=True)
+        id_high_planck += len(l_planck)
+        std_planck = np.sqrt(variance_planck)[id_low_planck:id_high_planck]
+
         lth, cl_th_and_fg = np.loadtxt("%s/best_fit_%sx%s_%s.dat" % (bestfit_dir, f0, f1, spec), unpack=True)
         lb, cb = planck_utils.binning(lth, cl_th_and_fg, lmax, binning_file=binning_file)
-
+        ps_name = "%s_%sx%s" % (spec, f0, f1)
+        lmin_c, lmax_c = lrange[ps_name]
         id=np.where((lb >= lmin_c) & (lb <= lmax_c))
-        
         lb, cb = lb[id], cb[id]
-
-        bin_start, bin_stop = binrange[ps_name]
-        
-        std_planck = np.sqrt(variance_planck)[id_low_planck:id_high_planck]
-        std_mean = np.sqrt(cov.diagonal())[bin_start:bin_stop]
-        
         fac = lb * (lb + 1) / (2 * np.pi)
-        
-        plt.semilogy()
-        plt.errorbar(lplanck, std_planck, color=color_array[id_f])
-        plt.errorbar(lb, std_mean, color=color_array[id_f], fmt = ".")
 
-        id_low_planck += len(lplanck)
+        bin_start, bin_stop = bin_range[ps_name]
+        std_sim = np.sqrt(cov.diagonal())[bin_start:bin_stop]
+        mean_sim = mean_vec[bin_start:bin_stop]
+        
+        plt.figure(figsize=(12, 8))
+        plt.errorbar(lb, cb * fac, color="grey", label = "input theory")
+        plt.errorbar(lb, mean_sim * fac, std_sim * fac, color="red", fmt = "." , label = "mean %s %s GHzx %s GHz"%(spec, f0, f1), alpha=0.4)
+        plt.xlabel(r"$\ell$", fontsize=20)
+        plt.ylabel(r"$D_\ell$", fontsize=20)
+        plt.legend()
+        plt.savefig("%s/mean_spectra_%s_%sx%s.png" % (plot_dir, spec, f0, f1))
+        plt.clf()
+        plt.close()
+        
+        plt.figure(figsize=(12, 8))
+        plt.semilogy()
+        plt.errorbar(l_planck, std_planck, color="grey", label = "std planck")
+        plt.errorbar(lb, std_sim, color="red", fmt = ".", label = "std sim %s %s GHzx %s GHz"%(spec, f0, f1), alpha=0.4)
+        plt.xlabel(r"$\ell$", fontsize=20)
+        plt.ylabel(r"$\sigma_\ell$", fontsize=20)
+        plt.legend()
+        plt.savefig("%s/std_%s_%sx%s.png" % (plot_dir, spec, f0, f1))
+        plt.clf()
+        plt.close()
+
+        id_low_planck = id_high_planck
     
-    plt.show()

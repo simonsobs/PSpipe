9f9961d1b61fe58b407678ea1c9e3010dceb4dea
Xavier Garrido <xavier.garrido@gmail.com>
2020-03-19 14:47:06 +0100

    upgrade packages version

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ import versioneer
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-setup(name="pspipe",
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      author="Simons Observatory Collaboration Power Spectrum Task Force",
-      url="https://github.com/simonsobs/PSpipe",
-      description="Pipeline analysis for the SO LAT experiment",
-      long_description=readme,
-      license="BSD license",
-      python_requires=">=3.5",
-      zip_safe=True,
-      packages=find_packages(),
-      install_requires=[
-          "scipy",
-          "camb",
-          "pspy @ git+https://github.com/simonsobs/pspy@master#egg=pspy",
-          "pymaster @ git+https://github.com/LSSTDESC/NaMaster@easier_libsharp#egg=pymaster",
-          "mflike @ git+https://github.com/simonsobs/LAT_MFLike@master#egg=mflike",
-      ])
+setup(
+    name="pspipe",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Simons Observatory Collaboration Power Spectrum Task Force",
+    url="https://github.com/simonsobs/PSpipe",
+    description="Pipeline analysis for the SO LAT experiment",
+    long_description=readme,
+    license="BSD license",
+    python_requires=">=3.5",
+    zip_safe=True,
+    packages=find_packages(),
+    install_requires=[
+        "scipy",
+        "camb",
+        "pspy>=1.2.1",
+        "pymaster>=1.0.2",
+        "mflike @ git+https://github.com/simonsobs/LAT_MFLike@master#egg=mflike",
+    ])

51275264e7bbda4d99f8ca3e03b67fa887c12c32
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-06-04 10:43:26 +0200

    change selection, now based on geometry

--- project/data_analysis/python/get_spectra.py
+++ project/data_analysis/python/get_spectra.py
@@ -48,7 +48,7 @@ for sv in surveys:
         for k, map in enumerate(maps):
             if win_T.pixel == "CAR":
                 car_box = so_map.bounding_box_from_map(win_T) * 180 / np.pi
-                split = so_map.read_map(map, car_box=car_box)
+                split = so_map.read_map(map, geometry=win.data.geometry)
 
             elif win_T.pixel == "HEALPIX":
                 split = so_map.read_map(map)

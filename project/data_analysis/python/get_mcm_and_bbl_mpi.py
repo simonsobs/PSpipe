e719254db32cbfa2179081333708c59546a9f0ce
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-07-25 12:40:18 +0200

    mpi version of get_mcm

---
+++ project/data_analysis/python/get_mcm_and_bbl_mpi.py
@@ -0,0 +1,83 @@
+"""
+This script computes the mode coupling matrices and the binning matrices Bbl
+for the different surveys and arrays
+"""
+
+from pspy import so_map, so_mcm, pspy_utils, so_dict, so_mpi
+import sys
+
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+
+window_dir = "windows"
+mcm_dir = "mcms"
+plot_dir = "plots/windows/"
+
+pspy_utils.create_directory(mcm_dir)
+pspy_utils.create_directory(window_dir)
+pspy_utils.create_directory(plot_dir)
+
+surveys = d["surveys"]
+lmax = d["lmax"]
+
+if d["use_toeplitz"] == True:
+    print("we will use the toeplitz approximation")
+    l_exact, l_band, l_toep = 800, 2000, 2750
+else:
+    l_exact, l_band, l_toep = None, None, None
+
+
+na_list, nb_list, nc_list, nd_list = [], [], [], []
+n_mcms = 0
+for id_sv1, sv1 in enumerate(surveys):
+    arrays_1 = d["arrays_%s" % sv1]
+    for id_ar1, ar1 in enumerate(arrays_1):
+        for id_sv2, sv2 in enumerate(surveys):
+            arrays_2 = d["arrays_%s" % sv2]
+            for id_ar2, ar2 in enumerate(arrays_2):
+                # This ensures that we do not repeat redundant computations
+                if  (id_sv1 == id_sv2) & (id_ar1 > id_ar2) : continue
+                if  (id_sv1 > id_sv2) : continue
+                na_list += [sv1]
+                nb_list += [ar1]
+                nc_list += [sv2]
+                nd_list += [ar2]
+                n_mcms += 1
+                
+
+print("number of mcm matrices to compute : %s" % n_mcms)
+so_mpi.init(True)
+subtasks = so_mpi.taskrange(imin=0, imax=n_mcms - 1)
+print(subtasks)
+for task in subtasks:
+    task = int(task)
+    sv1, ar1, sv2, ar2 = na_list[task], nb_list[task], nc_list[task], nd_list[task]
+    
+    print("%s_%s x %s_%s" % (sv1, ar1, sv2, ar2))
+    
+    l, bl1 = pspy_utils.read_beam_file(d["beam_%s_%s" % (sv1, ar1)])
+    win1_T = so_map.read_map(d["window_T_%s_%s" % (sv1, ar1)])
+    win1_pol = so_map.read_map(d["window_pol_%s_%s" % (sv1, ar1)])
+    
+    win1_T.write_map("%s/window_T_%s_%s.fits" % (window_dir, sv1, ar1))
+    win1_pol.write_map("%s/window_pol_%s_%s.fits" % (window_dir, sv1, ar1))
+
+    l, bl2 = pspy_utils.read_beam_file(d["beam_%s_%s" % (sv2, ar2)])
+    win2_T = so_map.read_map(d["window_T_%s_%s" % (sv2, ar2)])
+    win2_pol = so_map.read_map(d["window_pol_%s_%s" % (sv2, ar2)])
+
+    mbb_inv, Bbl = so_mcm.mcm_and_bbl_spin0and2(win1=(win1_T, win1_pol),
+                                                win2=(win2_T, win2_pol),
+                                                bl1=(bl1, bl1),
+                                                bl2=(bl2, bl2),
+                                                binning_file=d["binning_file"],
+                                                niter=d["niter"],
+                                                lmax=d["lmax"],
+                                                type=d["type"],
+                                                l_exact=l_exact,
+                                                l_band=l_band,
+                                                l_toep=l_toep,
+                                                save_file="%s/%s_%sx%s_%s"%(mcm_dir, sv1, ar1, sv2, ar2))
+
+
+

f05b1301485a1787c913c50afa9f0f2d155d59df
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-07-26 09:27:32 +0200

    updates

--- project/data_analysis/python/data_analysis_utils.py
+++ project/data_analysis/python/data_analysis_utils.py
@@ -295,12 +295,14 @@ def fast_cov_coupling(sq_win_alms_dir,
     except:
         alm_TbTc = np.load("%s/alms_%sx%s.npy"  % (sq_win_alms_dir, nc_r, nb_r))
 
-
+    print(alm_TaTc)
+    print(alm_TaTc.shape)
     wcl = {}
     wcl["TaTcTbTd"] = hp.alm2cl(alm_TaTc, alm_TbTd)
     wcl["TaTdTbTc"] = hp.alm2cl(alm_TaTd, alm_TbTc)
     l = np.arange(len(wcl["TaTcTbTd"]))
-    
+    print(wcl["TaTcTbTd"])
+    print(l)
     wcl["TaTcTbTd"] *= (2 * l + 1) / (4 * np.pi)
     wcl["TaTdTbTc"] *= (2 * l + 1) / (4 * np.pi)
 

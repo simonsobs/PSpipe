6d52bb48e5b47b13be54cc4c66a72a9f3ee286b7
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-12-06 19:38:33 +0100

    add tools for covariance studies

---
+++ project/Planck_pspy/python/EB_planck_covariance.py
@@ -0,0 +1,238 @@
+from pspy import pspy_utils, so_dict, so_map, so_mpi, so_mcm, so_spectra, so_cov
+import numpy as np
+import healpy as hp
+import sys
+
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+
+windows_dir = "windows"
+mcms_dir = "mcms"
+spectra_dir = "spectra"
+ps_model_dir = "noise_model"
+cov_dir = "covariances"
+bestfit_dir = "best_fits"
+
+
+pspy_utils.create_directory(cov_dir)
+
+type = d["type"]
+freqs = d["freqs"]
+binning_file = d["binning_file"]
+lmax = d["lmax"]
+niter = d["niter"]
+
+exp = "Planck"
+
+spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+spin_pairs = ["spin0xspin0", "spin0xspin2", "spin2xspin0", "spin2xspin2"]
+
+bin_lo, bin_hi, bin_c, bin_size = pspy_utils.read_binning_file(binning_file, lmax)
+nbins = len(bin_hi)
+
+ps_all = {}
+nl_all = {}
+bl1,bl2 = {}, {}
+spec_name = []
+
+ns = {"Planck": 2}
+
+pixwin = hp.pixwin(2048)[:lmax]
+
+for c1,freq1 in enumerate(freqs):
+    for c2,freq2 in enumerate(freqs):
+        if c1>c2: continue
+        
+        
+        l, bl1_hm1_T = np.loadtxt(d["beam_%s_hm1_T" % freq1], unpack=True)
+        l, bl1_hm2_T = np.loadtxt(d["beam_%s_hm2_T" % freq1], unpack=True)
+        l, bl1_hm1_pol = np.loadtxt(d["beam_%s_hm1_pol" % freq1], unpack=True)
+        l, bl1_hm2_pol = np.loadtxt(d["beam_%s_hm2_pol" % freq1], unpack=True)
+
+
+        l, bl2_hm1_T = np.loadtxt(d["beam_%s_hm1_T" % freq2], unpack=True)
+        l, bl2_hm2_T = np.loadtxt(d["beam_%s_hm2_T" % freq2], unpack=True)
+        l, bl2_hm1_pol = np.loadtxt(d["beam_%s_hm1_pol" % freq2], unpack=True)
+        l, bl2_hm2_pol = np.loadtxt(d["beam_%s_hm2_pol" % freq2], unpack=True)
+
+
+        bl1_hm1_T, bl1_hm2_T, bl2_hm1_T, bl2_hm2_T = bl1_hm1_T[2: lmax + 2], bl1_hm2_T[2: lmax + 2], bl2_hm1_T[2: lmax + 2], bl2_hm2_T[2: lmax + 2]
+        bl1_hm1_pol, bl1_hm2_pol, bl2_hm1_pol, bl2_hm2_pol = bl1_hm1_pol[2: lmax + 2], bl1_hm2_pol[2: lmax + 2], bl2_hm1_pol[2: lmax + 2], bl2_hm2_pol[2: lmax + 2]
+
+        bl1["TT"] = np.sqrt(bl1_hm1_T * bl1_hm2_T)
+        bl2["TT"] = np.sqrt(bl2_hm1_T * bl2_hm2_T)
+
+        bl1["EE"] = np.sqrt(bl1_hm1_pol * bl1_hm2_pol)
+        bl2["EE"] = np.sqrt(bl2_hm1_pol * bl2_hm2_pol)
+
+        bl1["TE"] = np.sqrt(bl1["EE"] * bl1["TT"])
+        bl2["TE"] = np.sqrt(bl2["EE"] * bl2["TT"])
+        
+        bl1["ET"] = bl1["TE"]
+        bl2["ET"] = bl2["TE"]
+
+        if d["use_noise_from_sim"]:
+            if d["use_ffp10"] == True:
+                spec_name_noise = "mean_simffp10_%s_%sx%s_%s_noise" % (exp, freq1, exp, freq2)
+            else:
+                spec_name_noise = "mean_sim_%s_%sx%s_%s_noise" % (exp, freq1, exp, freq2)
+        else:
+            spec_name_noise = "mean_%s_%sx%s_%s_noise" % (exp, freq1, exp, freq2)
+
+        
+        l, Nl = so_spectra.read_ps(ps_model_dir + "/%s.dat" % spec_name_noise, spectra=spectra)
+                
+        for spec in ["TT", "TE", "ET", "EE"]:
+            
+            if spec == "ET":
+                lth, ps_th = np.loadtxt("%s/best_fit_%sx%s_%s.dat"%(bestfit_dir, freq1, freq2, "TE"), unpack=True)
+            else:
+                lth, ps_th = np.loadtxt("%s/best_fit_%sx%s_%s.dat"%(bestfit_dir, freq1, freq2, spec), unpack=True)
+
+            ps_th = ps_th[2: lmax + 2]
+            
+            
+            ps_all["%s_%s" % (exp, freq1), "%s_%s" % (exp, freq2), spec] = bl1[spec] * bl2[spec] * pixwin**2 * ps_th
+                    
+            if freq1 == freq2:
+                nl_all["%s_%s" % (exp, freq1), "%s_%s" % (exp, freq2), spec] = Nl[spec] * ns[exp] * pixwin**2
+            else:
+                nl_all["%s_%s" % (exp, freq1), "%s_%s" % (exp, freq2), spec] = np.zeros(lmax)
+                    
+
+            ps_all["%s_%s" % (exp, freq2), "%s_%s"%(exp, freq1), spec] = ps_all["%s_%s"%(exp, freq1), "%s_%s"%(exp, freq2), spec]
+            nl_all["%s_%s" % (exp, freq2), "%s_%s"%(exp, freq1), spec] = nl_all["%s_%s"%(exp, freq1), "%s_%s"%(exp, freq2), spec]
+                
+        spec_name += ["%s_%sx%s_%s" % (exp, freq1, exp, freq2)]
+
+
+na_list, nb_list, nc_list, nd_list = [], [], [], []
+ncovs = 0
+
+for sid1, spec1 in enumerate(spec_name):
+    for sid2, spec2 in enumerate(spec_name):
+        if sid1 > sid2: continue
+        print (spec1,spec2)
+        na, nb = spec1.split("x")
+        nc, nd = spec2.split("x")
+        na_list += [na]
+        nb_list += [nb]
+        nc_list += [nc]
+        nd_list += [nd]
+        ncovs += 1
+
+nspecs=len(spec_name)
+
+print("number of covariance matrices to compute : %s" % ncovs)
+so_mpi.init(True)
+subtasks = so_mpi.taskrange(imin=0, imax=ncovs - 1)
+
+for task in subtasks:
+    task = int(task)
+    
+    na, nb, nc, nd = na_list[task], nb_list[task], nc_list[task], nd_list[task]
+    win = {}
+    win["Ta"] = so_map.read_map("%s/window_T_%s-hm1.fits"%(windows_dir, na))
+    win["Tb"] = so_map.read_map("%s/window_T_%s-hm2.fits"%(windows_dir, nb))
+    win["Tc"] = so_map.read_map("%s/window_T_%s-hm1.fits"%(windows_dir, nc))
+    win["Td"] = so_map.read_map("%s/window_T_%s-hm2.fits"%(windows_dir, nd))
+    win["Pa"] = so_map.read_map("%s/window_P_%s-hm1.fits"%(windows_dir, na))
+    win["Pb"] = so_map.read_map("%s/window_P_%s-hm2.fits"%(windows_dir, nb))
+    win["Pc"] = so_map.read_map("%s/window_P_%s-hm1.fits"%(windows_dir, nc))
+    win["Pd"] = so_map.read_map("%s/window_P_%s-hm2.fits"%(windows_dir, nd))
+
+    coupling = so_cov.cov_coupling_spin0and2_simple(win, lmax, niter=niter, planck=True)
+    analytic_cov = np.zeros((4*nbins, 4*nbins))
+
+    # EaEbEcEd
+    M_00 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EEEE")
+    M_00 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EEEE")
+    analytic_cov[0*nbins:1*nbins, 0*nbins:1*nbins] = so_cov.bin_mat(M_00, binning_file, lmax)
+    
+    # EaBbEcBd
+    M_11 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EEBB")
+    M_11 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EBBE")
+    analytic_cov[1*nbins:2*nbins, 1*nbins:2*nbins] = so_cov.bin_mat(M_11, binning_file, lmax)
+    
+    # BaEbBcEd
+    M_22 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BBEE")
+    M_22 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BEEB")
+    analytic_cov[2*nbins:3*nbins, 2*nbins:3*nbins] = so_cov.bin_mat(M_22, binning_file, lmax)
+    
+    # BaBbBcBd
+    M_33 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BBBB")
+    M_33 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BBBB")
+    analytic_cov[3*nbins:4*nbins, 3*nbins:4*nbins] = so_cov.bin_mat(M_33, binning_file, lmax)
+    
+    # EaEbEcBd
+    M_01 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EEEB")
+    M_01 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EBEE")
+    analytic_cov[0*nbins:1*nbins, 1*nbins:2*nbins] = so_cov.bin_mat(M_01, binning_file, lmax)
+    
+    # EaEbBcEd
+    M_02 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EBEE")
+    M_02 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EEEB")
+    analytic_cov[0*nbins:1*nbins, 2*nbins:3*nbins] = so_cov.bin_mat(M_02, binning_file, lmax)
+    
+    # EaEbBcBd
+    M_03 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EBEB")
+    M_03 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EBEB")
+    analytic_cov[0*nbins:1*nbins, 3*nbins:4*nbins] = so_cov.bin_mat(M_03, binning_file, lmax)
+    
+    # EaBbBcEd
+    M_12 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EBBE")
+    M_12 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EEBB")
+    analytic_cov[1*nbins:2*nbins, 2*nbins:3*nbins] = so_cov.bin_mat(M_12, binning_file, lmax)
+    
+    # EaBbBcBd
+    M_13 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EBBB")
+    M_13 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EBBB")
+    analytic_cov[1*nbins:2*nbins, 3*nbins:4*nbins] = so_cov.bin_mat(M_13, binning_file, lmax)
+    
+    # BaEbBcBd
+    M_23 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BBEB")
+    M_23 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BBEB")
+    analytic_cov[2*nbins:3*nbins, 3*nbins:4*nbins] = so_cov.bin_mat(M_23, binning_file, lmax)
+    
+    # EaBbEcEd
+    M_10 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "EEBE")
+    M_10 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "EEBE")
+    analytic_cov[1*nbins:2*nbins, 0*nbins:1*nbins] = so_cov.bin_mat(M_10, binning_file, lmax)
+    
+    # BaEbEcEd
+    M_20 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BEEE")
+    M_20 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BEEE")
+    analytic_cov[2*nbins:3*nbins, 0*nbins:1*nbins] = so_cov.bin_mat(M_20, binning_file, lmax)
+    
+    # BaBbEcEd
+    M_30 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BEBE")
+    M_30 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BEBE")
+    analytic_cov[3*nbins:4*nbins, 0*nbins:1*nbins] = so_cov.bin_mat(M_30, binning_file, lmax)
+    
+    # BaEbEcBd
+    M_21 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BEEB")
+    M_21 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BBEE")
+    analytic_cov[2*nbins:3*nbins, 1*nbins:2*nbins] = so_cov.bin_mat(M_21, binning_file, lmax)
+    
+    # BaBbEcBd
+    M_31 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BEBB")
+    M_31 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BBBE")
+    analytic_cov[3*nbins:4*nbins, 1*nbins:2*nbins] = so_cov.bin_mat(M_31, binning_file, lmax)
+    
+    # BaBbBcEd
+    M_32 = coupling["PaPcPbTP"] * so_cov.chi(na, nc, nb, nd, ns, ps_all, nl_all, "BBBE")
+    M_32 += coupling["PaPdPbPc"] * so_cov.chi(na, nd, nb, nc, ns, ps_all, nl_all, "BEBB")
+    analytic_cov[3*nbins:4*nbins, 2*nbins:3*nbins] = so_cov.bin_mat(M_32, binning_file, lmax)
+    
+    mbb_inv_ab, Bbl_ab = so_mcm.read_coupling(prefix="%s/%sx%s-hm1xhm2" % (mcms_dir, na, nb), spin_pairs=spin_pairs)
+    mbb_inv_ab = so_cov.extract_EEEBBB_mbb(mbb_inv_ab)
+    
+    mbb_inv_cd, Bbl_cd = so_mcm.read_coupling(prefix="%s/%sx%s-hm1xhm2" % (mcms_dir, nc, nd), spin_pairs=spin_pairs)
+    mbb_inv_cd = so_cov.extract_EEEBBB_mbb(mbb_inv_cd)
+
+    analytic_cov = np.dot(np.dot(mbb_inv_ab, analytic_cov), mbb_inv_cd.T)
+    
+    np.save("%s/analytic_cov_%sx%s_%sx%s_EB.npy" % (cov_dir, na, nb, nc, nd), analytic_cov)
+
+
+
---
+++ project/Planck_pspy/python/EB_planck_spectra_model.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pylab as plt
+from pspy import so_dict, so_spectra, pspy_utils
+import sys
+import scipy.interpolate
+import planck_utils
+
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+
+spectra_dir = "spectra"
+plot_dir = "plots"
+bestfit_dir = "best_fits"
+plot_dir = "plots"
+
+pspy_utils.create_directory(bestfit_dir)
+pspy_utils.create_directory(plot_dir)
+
+
+spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+type = d["type"]
+freqs = d["freqs"]
+binning_file = d["binning_file"]
+lmax = 2500
+splits = ["hm1", "hm2"]
+size = 1
+exp = "Planck"
+
+
+color_array = ["blue", "red", "green", "orange", "purple", "pink"]
+
+clth = {}
+lth2, clth["TT"], clth["EE"], clth["BB"], clth["TE"] =np.loadtxt("cosmo2017_10K_acc3_lensedCls.dat", unpack=True)
+clth["EB"] = 0
+
+for spec in ["EE", "BB", "EB"]:
+    clth[spec] *= 2 * np.pi/(lth2 * (lth2 + 1))
+
+    id_color = 0
+    for f1, freq1 in enumerate(freqs):
+        for f2, freq2 in enumerate(freqs):
+            if f1 > f2: continue
+            fname = "%sx%s" % (freq1, freq2)
+
+            spec_name = "%s_%sx%s_%s-%sx%s" % (exp, freq1, exp, freq2, "hm1", "hm2")
+            l, ps = so_spectra.read_ps("%s/spectra_unbin_%s.dat" % (spectra_dir, spec_name), spectra=spectra)
+            
+            clth[spec] = clth[spec][:lmax]
+            l = l[:lmax]
+            ps[spec] = ps[spec][:lmax]
+            if spec is not "EB":
+                fg = ps[spec] - clth[spec]
+                fg_th = np.zeros(lmax)
+                pivot = 75
+                power1 = -1.9
+                power2 = -2.5
+                lmin_fit = 30
+                lmax_fit = 800
+            
+                A_amplitude = np.linspace(0.1, 100, 1000)
+                chi2 = np.zeros(1000)
+                for c, A in enumerate(A_amplitude):
+                    fg_th[lmin_fit:pivot] = (l[lmin_fit:pivot]/A)**(power1)*(l[pivot]/A)**(power2-power1)
+                    fg_th[pivot:] = (l[pivot:]/A)**(power2)
+                    chi2[c] = np.sum((fg_th[lmin_fit:lmax_fit]-fg[lmin_fit:lmax_fit])**2)
+            
+                id = np.where(chi2 == np.min(chi2))
+                A_bestfit = A_amplitude[id]
+                print(f1,f2,A_bestfit)
+            
+                fg_th[:pivot] = (l[:pivot]/A_bestfit)**(power1)*(l[pivot]/A_bestfit)**(power2-power1)
+                fg_th[pivot:] = (l[pivot:]/A_bestfit)**(power2)
+            else:
+                fg_th = np.zeros(len(l))
+            
+            np.savetxt("%s/best_fit_%s_%s.dat" % (bestfit_dir, fname, spec), np.transpose([l, (clth[spec] + fg_th)]))
+
+            model = (clth[spec] + fg_th) * l * (l +1) / (2 * np.pi)
+            ps[spec] *= (l * (l +1) / (2 * np.pi))
+            if spec != "EB":
+                plt.semilogy()
+            plt.plot(l[lmin_fit:],  model[lmin_fit:], color=color_array[id_color])
+            plt.errorbar(l[lmin_fit:], ps[spec][lmin_fit:], label = "%s_%sx%s" % (spec, freq1, freq2), fmt=".", color=color_array[id_color], alpha=0.2)
+
+            id_color += 1
+
+    plt.legend()
+    plt.savefig("%s/best_fit_%s.png" % (plot_dir,spec))
+    plt.clf()
+    plt.close()
+
+
+
+

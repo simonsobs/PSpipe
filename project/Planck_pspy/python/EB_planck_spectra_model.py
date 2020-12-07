093b184d9df6dec5cf8d0513a83f3027d8b71c97
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-12-07 13:37:46 +0100

    change lmax

--- project/Planck_pspy/python/EB_planck_spectra_model.py
+++ project/Planck_pspy/python/EB_planck_spectra_model.py
@@ -45,12 +45,10 @@ for spec in ["EE", "BB", "EB"]:
             spec_name = "%s_%sx%s_%s-%sx%s" % (exp, freq1, exp, freq2, "hm1", "hm2")
             l, ps = so_spectra.read_ps("%s/spectra_unbin_%s.dat" % (spectra_dir, spec_name), spectra=spectra)
             
-            clth_c = clth[spec][:lmax - 2]
-            lc = l[:lmax - 2]
             #ps[spec] = ps[spec][:lmax]
             if spec is not "EB":
-                fg = ps[spec] - clth_c
-                fg_th = np.zeros(lmax)
+                fg = ps[spec] - clth[:len(ps[spec])]
+                fg_th = np.zeros(len(lth))
                 pivot = 75
                 power1 = -1.9
                 power2 = -2.5

a9154b3e2b41cce7a3b203edf245a31d9b17065d
xzackli <xzackli@gmail.com>
2021-05-11 17:32:46 -0400

    adjust slurm commands

--- project/Planck_cov/scripts/16core6hr.cmd
+++ project/Planck_cov/scripts/16core6hr.cmd
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+VAR1 = $(echo $1 | xargs)
+
 #
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=1      # total number of tasks per node
@@ -6,7 +9,7 @@
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH -t 6:00:00
 #SBATCH -p physics
-#SBATCH --output=output/R-.%j-$(echo $1 | xargs).out
+#SBATCH --output=output/R-.%j-$VAR1.out
 
 module load anaconda3
 export OMP_NUM_THREADS=16

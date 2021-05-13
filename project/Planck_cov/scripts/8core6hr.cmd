10371d029765b9ca7eddd283f900e8d5484578a8
xzackli <xzackli@gmail.com>
2021-05-13 16:08:40 -0400

    actually request 8 cores in slurm script

--- project/Planck_cov/scripts/8core6hr.cmd
+++ project/Planck_cov/scripts/8core6hr.cmd
@@ -2,15 +2,15 @@
 #
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=1      # total number of tasks per node
-#SBATCH --cpus-per-task=16       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=8       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH -t 6:00:00
 #SBATCH -p physics
 #SBATCH --output=output/R-%j.out
 
 module load anaconda3
-export OMP_NUM_THREADS=16
-export JULIA_NUM_THREADS=16
+export OMP_NUM_THREADS=8
+export JULIA_NUM_THREADS=8
 
 $1
 

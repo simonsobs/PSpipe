290e3b1afcba24c98fff61505b5b8be376c0da9f
laposta-l <laposta-l@ll-a2c1.in2p3.fr>
2021-06-03 10:00:20 +0200

    fix planck tf

--- project/data_analysis/python/get_spectra.py
+++ project/data_analysis/python/get_spectra.py
@@ -12,6 +12,7 @@ import numpy as np
 import sys
 import data_analysis_utils
 import time
+import healpy as hp
 
 d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
@@ -49,13 +50,13 @@ for sv in surveys:
         win_pol = so_map.read_map(d["window_pol_%s_%s" % (sv, ar)])
 
         window_tuple = (win_T, win_pol)
-        
+
         maps = d["maps_%s_%s" % (sv, ar)]
         nsplit[sv] = len(maps)
 
         cal = d["cal_%s_%s" % (sv, ar)]
         print("%s split of survey: %s, array %s"%(nsplit[sv], sv, ar))
-        
+
         if deconvolve_pixwin:
             # ok so this is a bit overcomplicated because we need to take into account CAR and HEALPIX
             # for CAR the pixel window function deconvolution is done in Fourier space and take into account
@@ -76,17 +77,17 @@ for sv in surveys:
 
             elif win_T.pixel == "HEALPIX":
                 pixwin_l[sv] = hp.pixwin(win_T.nside)
-                
+
         else:
             inv_pixwin_lxly = None
 
 
         t = time.time()
         for k, map in enumerate(maps):
-        
+
             if win_T.pixel == "CAR":
                 split = so_map.read_map(map, geometry=win_T.data.geometry)
-                
+
                 if d["src_free_maps"] == True:
                     point_source_map = so_map.read_map(map.replace("srcfree.fits", "model.fits"))
                     point_source_mask = so_map.read_map(d["ps_mask"])
@@ -102,22 +103,22 @@ for sv in surveys:
                     if deconvolve_pixwin:
                         print("WARNING: pixwin deconvolution in CAR WITHOUT kspace filter is not implemented")
                         sys.exit()
-                        
+
             elif win_T.pixel == "HEALPIX":
                 split = so_map.read_map(map)
-                
+
             split.data *= cal
             if d["remove_mean"] == True:
                 split = data_analysis_utils.remove_mean(split, window_tuple, ncomp)
-                
+
 
             master_alms[sv, ar, k] = sph_tools.get_alms(split, window_tuple, niter, lmax)
-            
+
             if d["use_kspace_filter"]:
                 # there is an extra normalisation for the FFT/IFFT bit
                 # note that we apply it here rather than at the FFT level because correcting the alm is faster than correcting the maps
                 master_alms[sv, ar, k] /= (split.data.shape[1]*split.data.shape[2])
-                
+
         print(time.time()- t)
 
 ps_dict = {}
@@ -126,13 +127,14 @@ _, _, lb, _ = pspy_utils.read_binning_file(binning_file, lmax)
 for id_sv1, sv1 in enumerate(surveys):
     arrays_1 = d["arrays_%s" % sv1]
     nsplits_1 = nsplit[sv1]
-    
+
     if d["tf_%s" % sv1] is not None:
         print("will deconvolve tf of %s" %sv1)
         _, _, tf1, _ = np.loadtxt(d["tf_%s" % sv1], unpack=True)
+        tf1 = tf1[:len(lb)]
     else:
         tf1 = np.ones(len(lb))
-    
+
     if deconvolve_pixwin:
         # we have an extra correction for the 1d healpix pixel window function
         # this should be checked with simulations since maybe this
@@ -140,19 +142,20 @@ for id_sv1, sv1 in enumerate(surveys):
         l_pw = np.arange(len(pixwin_l[sv1]))
         _, pw1 = pspy_utils.naive_binning(l_pw,  pixwin_l[sv1], binning_file, lmax)
         tf1 *= pw1
-        
+
     for id_ar1, ar1 in enumerate(arrays_1):
-    
+
         for id_sv2, sv2 in enumerate(surveys):
             arrays_2 = d["arrays_%s" % sv2]
             nsplits_2 = nsplit[sv2]
-            
+
             if d["tf_%s" % sv2] is not None:
                 print("will deconvolve tf of %s" %sv2)
                 _, _, tf2, _ = np.loadtxt(d["tf_%s" % sv2], unpack=True)
+                tf2 = tf2[:len(lb)]
             else:
                 tf2 = np.ones(len(lb))
-                
+
             if deconvolve_pixwin:
                 l_pw = np.arange(len(pixwin_l[sv2]))
                 _, pw2 = pspy_utils.naive_binning(l_pw,  pixwin_l[sv2], binning_file, lmax)
@@ -167,20 +170,20 @@ for id_sv1, sv1 in enumerate(surveys):
                 for spec in spectra:
                     ps_dict[spec, "auto"] = []
                     ps_dict[spec, "cross"] = []
-                
+
                 for s1 in range(nsplits_1):
                     for s2 in range(nsplits_2):
                         if (sv1 == sv2) & (ar1 == ar2) & (s1>s2) : continue
-                    
+
                         mbb_inv, Bbl = so_mcm.read_coupling(prefix="%s/%s_%sx%s_%s" % (mcm_dir, sv1, ar1, sv2, ar2),
                                                             spin_pairs=spin_pairs)
 
                         l, ps_master = so_spectra.get_spectra_pixell(master_alms[sv1, ar1, s1],
                                                                      master_alms[sv2, ar2, s2],
                                                                      spectra=spectra)
-                                                              
+
                         spec_name="%s_%s_%sx%s_%s_%d%d" % (type, sv1, ar1, sv2, ar2, s1, s2)
-                        
+
                         lb, ps = so_spectra.bin_spectra(l,
                                                         ps_master,
                                                         binning_file,
@@ -188,7 +191,7 @@ for id_sv1, sv1 in enumerate(surveys):
                                                         type=type,
                                                         mbb_inv=mbb_inv,
                                                         spectra=spectra)
-                                                        
+
                         data_analysis_utils.deconvolve_tf(lb, ps, tf1, tf2, ncomp, lmax)
 
                         if write_all_spectra:
@@ -211,7 +214,7 @@ for id_sv1, sv1 in enumerate(surveys):
                 for spec in spectra:
                     ps_dict_cross_mean[spec] = np.mean(ps_dict[spec, "cross"], axis=0)
                     spec_name_cross = "%s_%s_%sx%s_%s_cross" % (type, sv1, ar1, sv2, ar2)
-                    
+
                     if ar1 == ar2 and sv1 == sv2:
                         # Average TE / ET so that for same array same season TE = ET
                         ps_dict_cross_mean[spec] = (np.mean(ps_dict[spec, "cross"], axis=0) + np.mean(ps_dict[spec[::-1], "cross"], axis=0)) / 2.
@@ -223,9 +226,7 @@ for id_sv1, sv1 in enumerate(surveys):
                         spec_name_noise = "%s_%s_%sx%s_%s_noise" % (type, sv1, ar1, sv2, ar2)
 
                 so_spectra.write_ps(specDir + "/%s.dat" % spec_name_cross, lb, ps_dict_cross_mean, type, spectra=spectra)
-                
+
                 if sv1 == sv2:
                     so_spectra.write_ps(specDir+"/%s.dat" % spec_name_auto, lb, ps_dict_auto_mean, type, spectra=spectra)
                     so_spectra.write_ps(specDir+"/%s.dat" % spec_name_noise, lb, ps_dict_noise_mean, type, spectra=spectra)
-
-

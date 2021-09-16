d841dde8eb14d778503b1ee1c49e22fd2f1f341c
alaposta <adrien.la.posta@gmail.com>
2021-09-16 22:14:34 +0200

    fix mc plot spectra

--- project/maps2params/python/maps_to_params_mc_plot_spectra.py
+++ project/maps2params/python/maps_to_params_mc_plot_spectra.py
@@ -20,7 +20,11 @@ lcut = d["lcut"]
 multistep_path = d["multistep_path"]
 include_fg = d["include_fg"]
 fg_dir = d["fg_dir"]
+
 fg_components = d["fg_components"]
+fg_components["tt"].remove("tSZ_and_CIB")
+for comp in ["tSZ", "cibc", "tSZxCIB"]:
+    fg_components["tt"].append(comp)
 
 specDir = "spectra"
 mcm_dir = "mcms"
@@ -46,17 +50,17 @@ for id_exp1, exp1 in enumerate(experiments):
         for id_exp2, exp2 in enumerate(experiments):
             freqs2 = d["freqs_%s"%exp2]
             for id_f2, f2 in enumerate(freqs2):
-                
+
                 if  (id_exp1 == id_exp2) & (id_f1 > id_f2) : continue
                 if  (id_exp1 > id_exp2) : continue
-    
+
                 l, bl1 = np.loadtxt("sim_data/beams/beam_%s_%s.dat" % (exp1, f1), unpack=True)
                 l, bl2 = np.loadtxt("sim_data/beams/beam_%s_%s.dat" % (exp2, f2), unpack=True)
 
                 if exp1 == exp2:
                     nl_file_t = "sim_data/noise_ps/noise_t_%s_%sx%s_%s.dat" % (exp1, f1, exp2, f2)
                     nl_file_pol = "sim_data/noise_ps/noise_pol_%s_%sx%s_%s.dat" % (exp1, f1, exp2, f2)
-                    
+
                     nlth = maps_to_params_utils.get_effective_noise(nl_file_t,
                                                                     bl1,
                                                                     bl2,
@@ -69,29 +73,29 @@ for id_exp1, exp1 in enumerate(experiments):
                         nlth[spec] = np.zeros(lmax)
 
                 prefix= "%s/%s_%sx%s_%s" % (mcm_dir, exp1, f1, exp2, f2)
-                
+
                 mbb_inv, Bbl = so_mcm.read_coupling(prefix=prefix,spin_pairs=spin_pairs)
 
                 for kind in ["cross", "noise", "auto"]:
                     ps_th = {}
                     for spec in spectra:
                         ps=Dlth[spec].copy()
-                        if spec == "TT":
-                            if include_fg:
-                                flth_all = 0
-                                for foreground in fg_components:
-                                    l, flth = np.loadtxt("%s/tt_%s_%sx%s.dat" % (fg_dir, foreground, f1, f2)
-                                                         ,unpack=True)
-                                    flth_all += flth[:lmax]
-                                ps = Dlth[spec] + flth_all
-                    
+                        #if spec == "TT":
+                        if include_fg:
+                            flth_all = 0
+                            for foreground in fg_components[spec.lower()]:
+                                l, flth = np.loadtxt("%s/%s_%s_%sx%s.dat" % (fg_dir,spec.lower(), foreground, f1, f2)
+                                                    ,unpack=True)
+                                flth_all += flth[:lmax]
+                            ps = Dlth[spec] + flth_all
+
                         if kind == "cross":
                             ps_th[spec] = ps
                         elif kind == "noise":
                             ps_th[spec] = nlth[spec] * lth**2 / (2 * np.pi)
                         elif kind == "auto":
                             ps_th[spec] = ps + nlth[spec] * lth**2 / (2 * np.pi) * ns[exp1]
-    
+
                     theory[exp1, f1, exp2, f2, kind] = ps_th
                     bin_theory[exp1, f1, exp2, f2, kind] = so_mcm.apply_Bbl(Bbl, ps_th, spectra=spectra)
 
@@ -125,27 +129,27 @@ for kind in ["cross", "noise", "auto"]:
                 for id_exp2, exp2 in enumerate(experiments):
                     freqs2 = d["freqs_%s" % exp2]
                     for id_f2, f2 in enumerate(freqs2):
-                        
+
                         if  (id_exp1 == id_exp2) & (id_f1 > id_f2) : continue
                         if  (id_exp1 > id_exp2) : continue
                         if (exp1 != exp2) & (kind == "noise"): continue
                         if (exp1 != exp2) & (kind == "auto"): continue
-                        
+
                         spec_name = "spectra_%s_%s_%sx%s_%s_%s" % (spec, exp1, f1, exp2, f2, kind)
 
                         lb, mean, std = np.loadtxt("%s/%s.dat" % (mc_dir, spec_name), unpack=True)
-                        
+
                         mean_dict[kind, spec, exp1, f1, exp2, f2] = mean
                         std_dict[kind, spec, exp1, f1, exp2, f2] = std
-                        
+
                         ps_th = theory[exp1, f1, exp2, f2, kind][spec]
                         ps_th_binned = bin_theory[exp1, f1, exp2, f2, kind][spec]
-                        
+
                         plt.figure(figsize=(8, 7))
-                
+
                         if spec == "TT":
                             plt.semilogy()
-                
+
                         plt.plot(lth, ps_th, color="grey", alpha=0.4)
                         plt.plot(lb, ps_th_binned)
                         plt.errorbar(lb, mean, std, fmt=".", color="red")
@@ -154,7 +158,7 @@ for kind in ["cross", "noise", "auto"]:
                         plt.savefig("%s/%s.png" % (plot_dir, spec_name), bbox_inches="tight")
                         plt.clf()
                         plt.close()
-                        
+
                         plt.errorbar(lb, mean - ps_th_binned, std / np.sqrt(nsims), fmt=".", color="red")
                         plt.title(r"$\Delta D^{%s,%s_{%s}x%s_{%s}}_{%s,\ell}$" % (spec, exp1, f1, exp2, f2, kind), fontsize=20)
                         plt.xlabel(r"$\ell$", fontsize=20)
@@ -168,9 +172,9 @@ for kind in ["cross", "noise", "auto"]:
                         plt.savefig("%s/frac_%s.png" % (plot_dir, spec_name), bbox_inches="tight")
                         plt.clf()
                         plt.close()
-                        
+
                         str = "%s.png" % (spec_name)
-                        
+
                         g.write('<div class=type>\n')
                         g.write('<img src="' + str + '" width="50%" /> \n')
                         g.write('<img src="' + 'diff_' + str + '" width="50%" /> \n')
@@ -193,7 +197,7 @@ for fig in ["log", "linear"]:
 
         plt.figure(figsize=(12, 12))
         color = iter(cm.rainbow(np.linspace(0, 1, n_spec["cross"] + 1)))
-    
+
         if fig == "log":
             plt.semilogy()
 
@@ -206,13 +210,13 @@ for fig in ["log", "linear"]:
                     for id_f2, f2 in enumerate(freqs2):
                         if  (id_exp1 == id_exp2) & (id_f1 > id_f2) : continue
                         if  (id_exp1 > id_exp2) : continue
-                
+
                         c = next(color)
-                        
+
                         mean = mean_dict["cross", spec, exp1, f1, exp2, f2]
                         std = std_dict["cross", spec, exp1, f1, exp2, f2]
                         ps_th = theory[exp1, f1, exp2, f2, "cross"][spec]
-                    
+
                         if (fig == "linear") and (spec == "TT"):
                             plt.errorbar(lb, mean * lb**2, std * lb**2, fmt='.', color=c, label="%s%s x %s%s" % (exp1, f1, exp2, f2), alpha=0.6)
                             plt.errorbar(lth, ps_th * lth**2, color=c, alpha=0.4)
@@ -242,8 +246,3 @@ for fig in ["log", "linear"]:
         plt.savefig("%s/all_%s_spectra_%s_all_%scross.png" % (plot_dir, fig, spec, exp_name), bbox_inches="tight")
         plt.clf()
         plt.close()
-
-
-
-
-

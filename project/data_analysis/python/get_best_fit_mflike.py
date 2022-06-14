5eb07b391bc38452fe4e8f6d8b255c1fdb3ad118
alaposta <adrien.la.posta@gmail.com>
2022-06-14 11:37:17 +0200

    update fg model

--- project/data_analysis/python/get_best_fit_mflike.py
+++ project/data_analysis/python/get_best_fit_mflike.py
@@ -10,6 +10,8 @@ import numpy as np
 import pylab as plt
 from pspy import pspy_utils, so_dict
 
+from itertools import product
+
 d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
 
@@ -48,19 +50,19 @@ for count, spec in enumerate(["TT", "EE", "BB", "TE" ]):
 clth["ET"] = clth["TE"]
 for spec in ["TB", "BT", "EB", "BE" ]:
     clth[spec] = clth["TT"] * 0
-    
+
 ell = np.arange(ell_min, ell_max)
 np.savetxt("%s/lcdm.dat" % bestfit_dir, np.transpose([ell, clth["TT"], clth["EE"], clth["BB"], clth["TE"]]))
-    
+
 # we will now use mflike (and in particular the fg module) to get the best fit foreground model
 # we will only include foreground in tt, note that for now only extragalactic foreground are present
 from mflike import theoryforge_MFLike as th_mflike
 ThFo = th_mflike.TheoryForge_MFLike()
+ThFo.bandint_freqs = np.array(freq_list, dtype = "float")
+ThFo.freqs = ThFo.bandint_freqs
 
 fg_norm = d["fg_norm"]
-fg_components =  d["fg_components"]
-components = {"tt": fg_components, "ee": [], "te": []}
-fg_model =  {"normalisation":  fg_norm, "components": components}
+fg_model =  {"normalisation":  fg_norm, "components": d["fg_components"]}
 fg_params = d["fg_params"]
 
 ThFo.foregrounds = fg_model
@@ -82,7 +84,7 @@ for spec in spectra:
                 np.savetxt("%s/fg_%s.dat" % (bestfit_dir, name),
                                     np.transpose([ell, fg_dict["tt", "all", freq1, freq2]]))
 
-                    
+
             np.savetxt("%s/best_fit_%s.dat" % (bestfit_dir, name),
                                 np.transpose([ell, cl_th_and_fg]))
 
@@ -90,4 +92,40 @@ for spec in spectra:
     plt.legend()
     plt.savefig("%s/best_fit_%s.png" % (plot_dir, spec))
     plt.clf()
+    plt.close()
+
+nfreq = len(freq_list)
+
+fg_model["components"]["tt"].remove("tSZ_and_CIB")
+for comp in ["tSZ", "cibc", "tSZxCIB"]:
+    fg_model["components"]["tt"].append(comp)
+
+for mode in ["tt", "te", "ee"]:
+    fig, axes = plt.subplots(nfreq, nfreq, sharex = True, sharey = True, figsize = (10, 10))
+    for i, cross in enumerate(product(freq_list, freq_list)):
+        f0, f1 = cross
+        f0, f1 = int(f0), int(f1)
+        idx = (i % nfreq, i // nfreq)
+        ax = axes[idx]
+
+        if idx in zip(*np.triu_indices(nfreq, k=1)) and mode != "te":
+            fig.delaxes(ax)
+            continue
+
+        for comp in fg_model["components"][mode]:
+            ax.plot(ell, fg_dict[mode, comp, f0, f1])
+        ax.plot(ell, fg_dict[mode, "all", f0, f1], color = "k")
+        ax.plot(ell, clth[mode.upper()], color = "gray")
+        ax.legend([], title="{}x{} GHz".format(*cross))
+        if mode == "tt":
+            ax.set_yscale("log")
+            ax.set_ylim(1e-1, 1e4)
+        if mode == "ee":
+            ax.set_yscale("log")
+    for i in range(nfreq):
+        axes[-1, i].set_xlabel(r"$\ell$")
+        axes[i, 0].set_ylabel(r"$D_\ell$")
+    fig.legend(fg_model["components"][mode] + ["all"], title=mode.upper(), bbox_to_anchor=(1,1))
+    plt.tight_layout()
+    plt.savefig("%s/foregrounds_all_comps_%s.png"%(plot_dir,mode), dpi = 300)
     plt.close()

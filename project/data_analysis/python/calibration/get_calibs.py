d198c338e5247d325dd18740cac1268c7ca5b55d
alaposta <adrien.la.posta@gmail.com>
2022-06-22 16:20:49 +0200

    update calibs script for pspipe_utils

--- project/data_analysis/python/calibration/get_calibs.py
+++ project/data_analysis/python/calibration/get_calibs.py
@@ -1,8 +1,8 @@
-from pspy import pspy_utils, so_dict, so_consistency, so_spectra, so_cov
+from pspy import pspy_utils, so_dict, so_spectra, so_cov
+from pspipe_utils import consistency
 import numpy as np
 import pickle
 import sys
-
 
 d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
@@ -53,6 +53,7 @@ pspy_utils.create_directory(residual_output_dir)
 chains_dir = f"{output_dir}/chains"
 pspy_utils.create_directory(chains_dir)
 
+
 # Define the multipole range used to obtain
 # the calibration amplitudes
 multipole_range = {"dr6_pa4_f150": [1250, 1800],
@@ -61,7 +62,6 @@ multipole_range = {"dr6_pa4_f150": [1250, 1800],
                    "dr6_pa5_f150": [1250, 1800],
                    "dr6_pa6_f090": [800, 1100],
                    "dr6_pa6_f150": [1250, 1800]}
-pickle.dump(multipole_range, open(f"{output_dir}/multipole_range.pkl", "wb"))
 
 # Define the reference arrays
 ref_arrays = {"dr6_pa4_f150": "Planck_f143",
@@ -70,12 +70,11 @@ ref_arrays = {"dr6_pa4_f150": "Planck_f143",
              "dr6_pa5_f150": "Planck_f143",
              "dr6_pa6_f090": "Planck_f100",
              "dr6_pa6_f150": "Planck_f143"}
-pickle.dump(ref_arrays, open(f"{output_dir}/reference_arrays.pkl", "wb"))
 
-calib_dict = {}
+results_dict = {}
 for ar in d["arrays_dr6"]:
-
     array = f"dr6_{ar}"
+    results_dict[array] = {}
     ref_array = ref_arrays[array]
 
     spectra_order = [(array, array, "TT"),
@@ -96,30 +95,33 @@ for ar in d["arrays_dr6"]:
             cov_dict[(ar1, ar2, m1), (ar3, ar4, m2)] = cov
 
     # Concatenation
-    spec_vec, full_cov = so_consistency.append_spectra_and_cov(ps_dict, cov_dict, spectra_order)
+    spec_vec, full_cov = consistency.append_spectra_and_cov(ps_dict, cov_dict, spectra_order)
 
     # Save and plot residuals before calibration
-    res_spectrum, res_cov = so_consistency.project_spectra_vec_and_cov(spec_vec, full_cov, proj_pattern)
+    res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov, proj_pattern)
     np.savetxt(f"{residual_output_dir}/residual_{array}_before.dat", np.array([lb, res_spectrum]).T)
     np.savetxt(f"{residual_output_dir}/residual_cov_{array}.dat", res_cov)
 
     lmin, lmax = multipole_range[array]
     id = np.where((lb >= lmin) & (lb <= lmax))[0]
-    so_consistency.plot_residual(lb[id], res_spectrum[id], res_cov[np.ix_(id, id)],
+    consistency.plot_residual(lb[id], res_spectrum[id], res_cov[np.ix_(id, id)],
                      "TT", array, f"{residual_output_dir}/residual_{array}_before")
 
     # Calibrate the spectra
-    cal_mean, cal_std = so_consistency.get_calibration_amplitudes(spec_vec, full_cov,
+    cal_mean, cal_std = consistency.get_calibration_amplitudes(spec_vec, full_cov,
                                                     proj_pattern, "TT", id,
                                                     f"{chains_dir}/{array}")
-    calib_dict[array] = [cal_mean, cal_std]
+
+    results_dict[array] = {"multipole_range": multipole_range[array],
+                           "ref_array": ref_arrays[array],
+                           "calibs": [cal_mean, cal_std]}
 
     calib_vec = np.array([cal_mean**2, cal_mean, 1])
-    res_spectrum, res_cov = so_consistency.project_spectra_vec_and_cov(spec_vec, full_cov,
+    res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov,
                                                           proj_pattern,
                                                           calib_vec = calib_vec)
     np.savetxt(f"{residual_output_dir}/residual_{array}_after.dat", np.array([lb, res_spectrum]).T)
-    so_consistency.plot_residual(lb[id], res_spectrum[id], res_cov[np.ix_(id, id)],
+    consistency.plot_residual(lb[id], res_spectrum[id], res_cov[np.ix_(id, id)],
                      "TT", array, f"{residual_output_dir}/residual_{array}_after")
 
-pickle.dump(calib_dict, open(f"{output_dir}/calibs_dict.pkl", "wb"))
+pickle.dump(results_dict, open(f"{output_dir}/calibs_dict.pkl", "wb"))

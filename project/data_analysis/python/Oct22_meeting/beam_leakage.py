573f908114852dd93da48c7af9da3aa7fc48a1ab
Thibaut Louis <thbtlouis@gmail.com>
2022-10-20 07:54:45 -0400

    cleaner version

--- project/data_analysis/python/Oct22_meeting/beam_leakage.py
+++ project/data_analysis/python/Oct22_meeting/beam_leakage.py
@@ -10,12 +10,11 @@ def interp(l, l_s, fs):
 
 spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
 
-arrays = ["pa5_f090", "pa6_f090"]
-field = "TE"
+arrays = ["pa6_f090", "pa5_f090"]
 lmax_beam = 6000
 nsims  = 200
 
-ps, mean, std, gamma, gamma_s = {}, {}, {}, {}, {}
+gamma, gamma_s = {}, {}
 for ar in arrays:
 
     beam = np.loadtxt(f"beams_adri/coadd_{ar}_night_beam_tform_jitter_cmb.txt")
@@ -32,45 +31,70 @@ for ar in arrays:
     gamma[ar, "TB"] = bl_TB / bl
 
     l_s, bl_s, bl_TE_s, bl_TB_s =  np.loadtxt(f"beams_leakage_sigurd/uranus_{ar}_night_uranus_map0300_beam_tform.txt", unpack=True)
-    bl_s = interp(l, l_s, bl_s)
-    bl_TE_s = interp(l, l_s, bl_TE_s)
-    bl_TB_s = interp(l, l_s, bl_TB_s)
+    bl_s_interp = interp(l, l_s, bl_s)
+    bl_TE_s_interp = interp(l, l_s, bl_TE_s)
+    bl_TB_s_interp = interp(l, l_s, bl_TB_s)
 
-    gamma_s[ar, "TE"] = bl_TE_s / bl_s
-    gamma_s[ar, "TB"] = bl_TB_s / bl_s
+    gamma_s[ar, "TE"] = bl_TE_s_interp / bl_s_interp
+    gamma_s[ar, "TB"] = bl_TB_s_interp / bl_s_interp
 
-    plt.plot(l, bl_s/bl_s[0])
-    plt.plot(l, bl/bl[0])
+    plt.figure(figsize=(12, 8))
+
+    id = np.where(l_s<lmax_beam)
+    plt.plot(l, bl_s_interp, color="blue", label="Sig")
+    plt.plot(l_s[id], bl_s[id], "o", color="blue")
+    plt.plot(l, bl / bl[0], color="red", label="Adri")
+    plt.ylabel(r"$B_{\ell}$", fontsize=20)
+    plt.xlabel(r"$\ell$", fontsize=20)
+    plt.legend(fontsize=16)
     plt.show()
 
-    plt.plot(l, gamma[ar, "TE"])
-    plt.plot(l, gamma_s[ar, "TE"])
+    plt.figure(figsize=(12, 8))
+    plt.plot(l, gamma_s[ar, "TE"], color="blue", label="Sig")
+    plt.plot(l_s[id], bl_TE_s[id]/bl_s[id], "o", color="blue")
+    plt.plot(l, gamma[ar, "TE"], color="red", label="Adri")
+    plt.ylabel(r"$\gamma^{\rm TE}$", fontsize=20)
+    plt.xlabel(r"$\ell$", fontsize=20)
+    plt.legend(fontsize=16)
     plt.show()
+    
+    plt.figure(figsize=(12, 8))
+    plt.plot(l, gamma_s[ar, "TB"], color="blue", label="Sig")
+    plt.plot(l_s[id], bl_TB_s[id]/bl_s[id], "o", color="blue")
+    plt.plot(l, gamma[ar, "TB"], color="red", label="Adri")
+    plt.ylabel(r"$\gamma^{\rm TB}$", fontsize=20)
+    plt.xlabel(r"$\ell$", fontsize=20)
+    plt.legend(fontsize=16)
+    plt.show()
+
 
 
 ar0, ar1 = arrays
 lb, Db0 = so_spectra.read_ps(f"spectra/Dl_dr6_{ar0}xdr6_{ar0}_cross.dat", spectra=spectra)
 lb, Db1 = so_spectra.read_ps(f"spectra/Dl_dr6_{ar1}xdr6_{ar1}_cross.dat", spectra=spectra)
 
-null =  Db1[field] - Db0[field]
-null_list = []
-for iii in range(nsims):
-    lb, Db0_sim = so_spectra.read_ps(f"sim_spectra/Dl_dr6_{ar0}xdr6_{ar0}_cross_%05d.dat" % iii, spectra=spectra)
-    lb, Db1_sim = so_spectra.read_ps(f"sim_spectra/Dl_dr6_{ar1}xdr6_{ar1}_cross_%05d.dat" % iii, spectra=spectra)
-    null_list += [Db1_sim[field] - Db0_sim[field]]
-
-mean = np.mean(null_list, axis=0)
-std = np.std(null_list, axis=0)
-
 data = np.loadtxt("cosmo2017_10K_acc3_lensedCls.dat")
 l, cl_TT = data[:,0], data[:,1]
 l, cl_TT = l[:lmax_beam], cl_TT[:lmax_beam]
 
-plt.figure(figsize=(16, 8))
-plt.errorbar(lb, lb * null, std * lb, fmt=".", label=f"null {ar1} - {ar0}")
-plt.plot(l, l * (gamma[ar1, field] - gamma[ar0, field]) * cl_TT, label="leakage std map maker (Adri)")
-plt.plot(l, l * (gamma_s[ar1, field] - gamma_s[ar0, field]) * cl_TT, label="leakage max like map maker (Sigurd)")
-plt.legend(fontsize=18)
-plt.savefig(f"leakage_{ar1}-{ar0}_{field}.png", bbox_inches="tight")
-plt.clf()
-plt.close()
+
+for field in ["TE", "TB"]:
+
+    null =  Db1[field] - Db0[field]
+    null_list = []
+    for iii in range(nsims):
+        lb, Db0_sim = so_spectra.read_ps(f"sim_spectra/Dl_dr6_{ar0}xdr6_{ar0}_cross_%05d.dat" % iii, spectra=spectra)
+        lb, Db1_sim = so_spectra.read_ps(f"sim_spectra/Dl_dr6_{ar1}xdr6_{ar1}_cross_%05d.dat" % iii, spectra=spectra)
+        null_list += [Db1_sim[field] - Db0_sim[field]]
+
+    mean = np.mean(null_list, axis=0)
+    std = np.std(null_list, axis=0)
+
+    plt.figure(figsize=(16, 8))
+    plt.errorbar(lb, null * lb, std * lb, fmt=".", label=f"null {ar1} - {ar0}")
+    plt.plot(l, l * (gamma[ar1, field] - gamma[ar0, field]) * cl_TT, label="leakage std map maker (Adri)")
+    plt.plot(l, l * (gamma_s[ar1, field] - gamma_s[ar0, field]) * cl_TT, label="leakage max like map maker (Sigurd)")
+    plt.legend(fontsize=18)
+    plt.savefig(f"leakage_{ar1}-{ar0}_{field}.png", bbox_inches="tight")
+    plt.clf()
+    plt.close()

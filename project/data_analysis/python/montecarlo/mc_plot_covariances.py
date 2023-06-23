6a80432ee94cc27f72b7a21a8bd9903173568d35
Xavier Garrido <xavier.garrido@gmail.com>
2023-06-23 17:09:03 +0200

    revert example

--- project/data_analysis/python/montecarlo/mc_plot_covariances.py
+++ project/data_analysis/python/montecarlo/mc_plot_covariances.py
@@ -1,21 +1,16 @@
 """
-This script compare analytical covariances and monte carlo covariances
+This script compare analytical covariances and monte carlo covariances 
 """
 
 
 
 import matplotlib
-
 matplotlib.use("Agg")
-import os
-import sys
-
-import numpy as np
-import pspipe
+from pspy import pspy_utils, so_dict, so_cov
+import  numpy as np
 import pylab as plt
+import os, sys
 from pspipe_utils import pspipe_list
-from pspy import pspy_utils, so_cov, so_dict
-
 
 def write_html(filename, spec_list, multistep_path, cov_plot_dir):
 
@@ -37,10 +32,10 @@ def write_html(filename, spec_list, multistep_path, cov_plot_dir):
     for sid1, spec1 in enumerate(spec_list):
         for sid2, spec2 in enumerate(spec_list):
             if sid1>sid2: continue
-
+            
             n1,n2=spec1.split('x')
             n3,n4=spec2.split('x')
-
+            
             str='%s_%s_%s.png'%(filename, spec1, spec2)
             g.write('<div class=sub>\n')
             g.write('<img src="'+str+'"  /> \n')
@@ -60,7 +55,7 @@ cov_plot_dir = "plots/covariance"
 surveys = d["surveys"]
 lmax = d["lmax"]
 binning_file = d["binning_file"]
-multistep_path = os.path.join(os.path.dirname(pspipe.__file__), "js")
+multistep_path = d["multistep_path"]
 
 pspy_utils.create_directory(cov_plot_dir)
 
@@ -70,13 +65,13 @@ spec_list = pspipe_list.get_spec_name_list(d, char="_")
 for sid1, spec1 in enumerate(spec_list):
     for sid2, spec2 in enumerate(spec_list):
         if sid1 > sid2 : continue
-
+        
         n1, n2 = spec1.split("x")
         n3, n4 = spec2.split("x")
-
+        
         analytic_cov = np.load(f"{cov_dir}/analytic_cov_{n1}x{n2}_{n3}x{n4}.npy")
         mc_cov = np.load(f"{cov_dir}/mc_cov_{n1}x{n2}_{n3}x{n4}.npy")
-
+        
         bin_lo, bin_hi, lb, bin_size = pspy_utils.read_binning_file(binning_file, lmax)
         n_bins = len(bin_hi)
 
@@ -88,10 +83,10 @@ for sid1, spec1 in enumerate(spec_list):
                    "TEEE", "ETEE",
                    "EETE", "EEET", "ETTE", "ETTT",
                    "EETT", "TETT"]:
-
+            
             mc_cov_sub = so_cov.selectblock(mc_cov, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
             analytic_cov_sub= so_cov.selectblock(analytic_cov, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
-
+            
             var = mc_cov_sub.diagonal()
             analytic_var = analytic_cov_sub.diagonal()
 
@@ -109,15 +104,15 @@ for sid1, spec1 in enumerate(spec_list):
         plt.savefig(f"{cov_plot_dir}/covariance_pseudo_diagonal_{spec1}_{spec2}.png", bbox_inches="tight")
         plt.clf()
         plt.close()
-
+        
         plt.figure(figsize=(15, 15))
         plt.suptitle(f"{spec1} {spec2} (press c/v to switch between covariance matrix elements)", fontsize=30)
         count = 1
         for bl in ["TTTT", "TETE", "ETET", "EEEE"]:
-
+            
             mc_cov_sub = so_cov.selectblock(mc_cov, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
             analytic_cov_sub= so_cov.selectblock(analytic_cov, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
-
+            
             var = mc_cov_sub.diagonal()
             analytic_var = analytic_cov_sub.diagonal()
 
@@ -132,7 +127,7 @@ for sid1, spec1 in enumerate(spec_list):
         plt.savefig(f"{cov_plot_dir}/covariance_pseudo_diagonal_ratio_{spec1}_{spec2}.png", bbox_inches="tight")
         plt.clf()
         plt.close()
-
+        
 
         analytic_corr=so_cov.cov2corr(analytic_cov)
         mc_corr=so_cov.cov2corr(mc_cov)
@@ -145,13 +140,13 @@ for sid1, spec1 in enumerate(spec_list):
                     "TEEE", "ETEE",
                     "EETE", "EEET", "ETTE", "ETTT",
                     "EETT", "TETT"]:
-
+            
             mc_corr_sub = so_cov.selectblock(mc_corr, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
             analytic_corr_sub= so_cov.selectblock(analytic_corr, ["TT", "TE", "ET", "EE"], n_bins, block=bl)
-
+            
             off_diag = mc_corr_sub.diagonal(1)
             analytic_off_diag = analytic_corr_sub.diagonal(1)
-
+            
             plt.subplot(3,6,count)
             plt.plot(off_diag[1:], ".", label="MC %sx%s" % (bl[:2],bl[2:4]))
             plt.plot(analytic_off_diag[1:], label = "Analytic %sx%s" % (bl[:2],bl[2:4]))
@@ -167,3 +162,15 @@ for sid1, spec1 in enumerate(spec_list):
 
 filename = "covariance_pseudo_diagonal"
 write_html(filename, spec_list, multistep_path, cov_plot_dir)
+
+
+
+
+
+
+
+
+
+
+
+

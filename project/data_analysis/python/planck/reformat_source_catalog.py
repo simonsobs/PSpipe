80b77b5f1831f95596d99f80fd7e3ffb5bb9ed17
adrien-laposta <adrien.la.posta@gmail.com>
2023-08-04 16:39:02 +0200

    script to reformat mf source catalog

---
+++ project/data_analysis/python/planck/reformat_source_catalog.py
@@ -0,0 +1,63 @@
+"""
+This script is used to reformat the ACT multifrequency
+point source catalog `cat_skn_multifreq_20220526_nightonly`
+in three different monofrequency catalogs (at 90,150 and 220 GHz)
+Main usage: run dory point sources subtraction for NPIPE maps
+"""
+from pixell import utils
+import pandas as pd
+import numpy as np
+
+ps_catalog_path="/global/cfs/cdirs/act/data/tlouis/s17s18s19/catalogs/cat_skn_multifreq_20220526_nightonly.txt"
+
+input_catalog = pd.read_table(ps_catalog_path, escapechar="#", sep="\s+")
+input_catalog = input_catalog.shift(1, axis=1)
+
+flux_id = {90: 1, 150: 2, 220: 3}
+
+# https://phy-wiki.princeton.edu/polwiki/pmwiki.php?n=BeamAnalysis.BeamDistributionCenter
+beam_areas = {
+    90: [484.17, 487.03],
+    150: [228.44, 215.24, 221.88], #nsr
+    220: [107.34]
+}
+
+for freq, freq_id in flux_id.items():
+
+    flux = {
+        m: getattr(input_catalog, f"flux_{m}{freq_id}") for m in ["T", "Q", "U"]
+    }
+    dflux = {
+        m: getattr(input_catalog, f"dflux_{m}{freq_id}") for m in ["T", "Q", "U"]
+    }
+
+    # nsr to sr
+    beam_area = np.mean(beam_areas[freq])/1e9
+
+    amp = {}
+    damp = {}
+    for m in ["T", "Q", "U"]:
+        # utils.dplanck converts Jy to Kelvins
+        # flux is given in mJy and we want amps in mK
+        amp[m] = flux[m] / (utils.dplanck(freq*1e9) * beam_area)
+        damp[m] = dflux[m] / (utils.dplanck(freq*1e9) * beam_area)
+
+    ca = input_catalog.ca
+    status = np.where(ca < 3, 1, 2)
+
+    snr = amp["T"] / damp["T"]
+    # npix is not used anywhere
+    npix = np.zeros_like(snr)
+
+    output_catalog = [input_catalog.ra, input_catalog.dec, snr, amp["T"], damp["T"], amp["Q"], damp["Q"], amp["U"], damp["U"],
+                      flux["T"], dflux["T"], flux["Q"], dflux["Q"], flux["U"], dflux["U"],
+                      npix, status]
+
+    output_catalog = np.transpose(output_catalog)
+
+    # ordering by snr
+    output_catalog = output_catalog[output_catalog[:,2].argsort()[::-1]]
+
+    header = "ra dec SNR Tamp dTamp Qamp dQamp Uamp dUamp Tflux dTflux Qflux dQflux Uflux dUflux npix status"
+    fmt = "%11.6f %11.6f %8.3f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %5.0f %2.0f"
+    np.savetxt(f"cat_skn_{freq:03d}_20220526_nightonly_ordered.txt", output_catalog, header=header, fmt=fmt)

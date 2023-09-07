fix npipe noise alms rotation

project/data_analysis/python/planck/get_planck_npipe_sim_nlms.py
@@ -4,9 +4,9 @@ noise simulations and to save them to disk in a format
 compatible with the requirements of `mc_mnms_get_spectra.py`
 Disk usage: 144M per alm
 """
-from pspy import so_dict, pspy_utils, so_mpi
+from pspy import so_dict, pspy_utils, so_mpi, sph_tools, so_map
+from pixell import curvedsky, reproject
 from pspipe_utils import log
-import healpy as hp
 import numpy as np
 import time
 import sys
@@ -16,6 +16,7 @@ d.read_from_file(sys.argv[1])
 log = log.get_logger(**d)
 
 lmax = d["lmax"]
+niter = d["niter"]
 sv = "Planck"
 planck_arrays = d[f"arrays_{sv}"]
 planck_splits = ["A", "B"]
@@ -27,7 +28,7 @@ pspy_utils.create_directory(output_dir)
 
 n_sims = d["iStop"] - d["iStart"] + 1
 
-mpi_list = [(f, s, iii) for f in planck_arrays for s in planck_splits for iii in range(n_sims)]
+mpi_list = [(f, s, iii) for f in planck_arrays for s in planck_splits for iii in range(d["iStart"], d["iStop"]+1)]
 
 so_mpi.init(True)
 subtasks = so_mpi.taskrange(imin=0, imax=len(mpi_list)-1)
@@ -43,11 +44,15 @@ for id_mpi in subtasks:
     # Note that and we are using the sim index 0 as the first simulation (corresponding to the 200th NPIPE sim.)
     map_name = f"{npipe_dir}/npipe6v20{split}_sim/{iii+200:04d}/residual/residual_npipe6v20{split}_{freq}_{iii+200:04d}.fits"
 
-    hp_map = hp.read_map(map_name, field=(0,1,2))
-    hp_map *= 1e6 # from K to uK
+    hp_map = so_map.read_map(map_name, coordinate="gal", fields_healpix=[0,1,2])
+    hp_map.data *= 1e6 # from K to uK
 
-    alms = hp.map2alm(hp_map, lmax=lmax)
+    alms = sph_tools.map2alm(hp_map, niter=niter, lmax=lmax, dtype=np.complex64)
 
+    # Rotate alms
+    rot=f"{hp_map.coordinate},equ"
+    curvedsky.rotate_alm(alms, *reproject.rot2euler(rot), inplace=True)
+    
     # Note that we use the PSpipe conventions : i.e. indexing splits with an integer (here 0,1)
     np.save(f"{output_dir}/nlms_{sv}_f{freq}_set{planck_splits.index(split)}_{iii:05d}.npy", alms)
 
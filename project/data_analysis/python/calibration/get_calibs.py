d476836a23c047c821aa94c80eacaaae0a18c947
Thibaut Louis <thbtlouis@gmail.com>
2023-09-22 13:33:21 +0200

    modernize this

--- project/data_analysis/python/calibration/get_calibs.py
+++ project/data_analysis/python/calibration/get_calibs.py
@@ -1,11 +1,31 @@
 from pspy import pspy_utils, so_dict, so_spectra, so_cov
-from pspipe_utils import consistency
+from pspipe_utils import consistency, log
 import numpy as np
+import pylab as plt
 import pickle
 import sys
 
+
+def get_proj_pattern(test, map_set, ref_map_set):
+
+    if test == "AxA-AxP":
+        proj_pattern = np.array([1, -1, 0])
+        #    [1, -1, 0]]  x [ AxA, AxP, PxP].T    =  AxA - AxP
+        name = f"{map_set}x{map_set}-{map_set}x{ref_map_set}"
+    elif test == "AxA-PxP":
+        proj_pattern = np.array([1, 0, -1])
+         #    [1, 0, -1]]  x [ AxA, AxP, PxP].T    =   AxA - PxP
+        name = f"{map_set}x{map_set}-{ref_map_set}x{ref_map_set}"
+    elif test == "PxP-AxP":
+        proj_pattern = np.array([0, -1, 1])
+        #    [[0, -1, 1]]   x [ AxA, AxP, PxP].T    =    PxP - AxP
+        name = f"{ref_map_set}x{ref_map_set}-{map_set}x{ref_map_set}"
+
+    return  name, proj_pattern
+    
 d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
+log = log.get_logger(**d)
 
 spec_dir = "spectra"
 cov_dir = "covariances"
@@ -14,42 +34,23 @@ _, _, lb, _ = pspy_utils.read_binning_file(d["binning_file"], d["lmax"])
 n_bins = len(lb)
 
 spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
-modes = ["TT", "TE", "ET", "EE"]
 
-# Define the projection pattern - i.e. which
-# spectra combination will be used to compute
-# the residuals with
-#   A: the array you want to calibrate
-#   B: the reference array
-spectra_combin = "AxA-AxB"
+if d["cov_T_E_only"] == True:
+    modes = ["TT", "TE", "ET", "EE"]
+else:
+    modes = spectra
 
-if spectra_combin == "AxA-AxB":
-    proj_pattern = np.array([1, -1, 0])
-    #                     [[ AxA,
-    #    [[1, -1, 0]]  x     AxP,    =  AxA - AxP
-    #                        PxP ]]
-elif spectra_combin == "AxA-BxB":
-    proj_pattern = np.array([1, 0, -1])
-    #                     [[ AxA,
-    #    [[1, 0, -1]]  x     AxP,    =  AxA - PxP
-    #                        PxP ]]
-
-elif spectra_combin == "BxB-AxB":
-    proj_pattern = np.array([0, -1, 1])
-    #                     [[ AxA,
-    #    [[0, -1, 1]]  x     AxP,    =  PxP - AxP
-    #                        PxP ]]
 
 # Create output dirs
-output_dir = f"calibration_results_{spectra_combin}"
-pspy_utils.create_directory(output_dir)
-
+output_dir = f"calibration_results"
 residual_output_dir = f"{output_dir}/residuals"
-pspy_utils.create_directory(residual_output_dir)
-
+plot_output_dir = f"{output_dir}/plots"
 chains_dir = f"{output_dir}/chains"
-pspy_utils.create_directory(chains_dir)
 
+pspy_utils.create_directory(output_dir)
+pspy_utils.create_directory(residual_output_dir)
+pspy_utils.create_directory(chains_dir)
+pspy_utils.create_directory(plot_output_dir)
 
 # Define the multipole range used to obtain
 # the calibration amplitudes
@@ -61,66 +62,121 @@ multipole_range = {"dr6_pa4_f150": [1250, 1800],
                    "dr6_pa6_f150": [600, 1800]}
 
 # Define the reference arrays
-ref_arrays = {"dr6_pa4_f150": "Planck_f143",
+ref_map_sets = {"dr6_pa4_f150": "Planck_f143",
              "dr6_pa4_f220": "Planck_f217",
              "dr6_pa5_f090": "Planck_f100",
              "dr6_pa5_f150": "Planck_f143",
              "dr6_pa6_f090": "Planck_f100",
              "dr6_pa6_f150": "Planck_f143"}
 
+
+y_lims = {
+    "TT": (-100000, 75000),
+}
+
+# Define the projection pattern - i.e. which
+# spectra combination will be used to compute
+# the residuals with
+#   A: the array you want to calibrate
+#   B: the reference array
+
 results_dict = {}
-for ar in d["arrays_dr6"]:
-    array = f"dr6_{ar}"
-    results_dict[array] = {}
-    ref_array = ref_arrays[array]
 
-    spectra_order = [(array, array, "TT"),
-                     (array, ref_array, "TT"),
-                     (ref_array, ref_array, "TT")]
 
-    # Load spectra and cov
-    ps_dict = {}
-    cov_dict = {}
-    for i, (ar1, ar2, m1) in enumerate(spectra_order):
-        _, ps = so_spectra.read_ps(f"{spec_dir}/Dl_{ar1}x{ar2}_cross.dat",
-                                   spectra = spectra)
-        ps_dict[ar1, ar2, m1] = ps[m1]
-        for j, (ar3, ar4, m2) in enumerate(spectra_order):
-            if j < i: continue
-            cov = np.load(f"{cov_dir}/analytic_cov_{ar1}x{ar2}_{ar3}x{ar4}.npy")
-            cov = so_cov.selectblock(cov, modes, n_bins = n_bins, block = m1 + m2)
-            cov_dict[(ar1, ar2, m1), (ar3, ar4, m2)] = cov
+tests = ["AxA-AxP", "AxA-PxP", "PxP-AxP"]
 
-    # Concatenation
-    spec_vec, full_cov = consistency.append_spectra_and_cov(ps_dict, cov_dict, spectra_order)
+for test in tests:
+    for ar in d["arrays_dr6"]:
+    
+        map_set = f"dr6_{ar}"
+        ref_map_set = ref_map_sets[map_set]
 
-    # Save and plot residuals before calibration
-    res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov, proj_pattern)
-    np.savetxt(f"{residual_output_dir}/residual_{array}_before.dat", np.array([lb, res_spectrum]).T)
-    np.savetxt(f"{residual_output_dir}/residual_cov_{array}.dat", res_cov)
+        name, proj_pattern = get_proj_pattern(test, map_set, ref_map_set)
 
-    lmin, lmax = multipole_range[array]
-    id = np.where((lb >= lmin) & (lb <= lmax))[0]
-    consistency.plot_residual(lb, res_spectrum, {"analytical": res_cov}, "TT", array,
-                              f"{residual_output_dir}/residual_{array}_before",
-                              lrange=id, l_pow=1)
+        results_dict[name] = {}
+        
+        spectra_for_cal = [(map_set, map_set, "TT"),
+                           (map_set, ref_map_set, "TT"),
+                           (ref_map_set, ref_map_set, "TT")]
 
-    # Calibrate the spectra
-    cal_mean, cal_std = consistency.get_calibration_amplitudes(spec_vec, full_cov,
-                                                    proj_pattern, "TT", id,
-                                                    f"{chains_dir}/{array}")
+        # Load spectra and cov
+        ps_dict = {}
+        cov_dict = {}
+        for i, (ms1, ms2, m1) in enumerate(spectra_for_cal):
+            _, ps = so_spectra.read_ps(f"{spec_dir}/Dl_{ms1}x{ms2}_cross.dat",
+                                       spectra = spectra)
+            ps_dict[ms1, ms2, m1] = ps[m1]
+            for j, (ms3, ms4, m2) in enumerate(spectra_for_cal):
+                if j < i: continue
+                cov = np.load(f"{cov_dir}/analytic_cov_{ms1}x{ms2}_{ms3}x{ms4}.npy")
+                cov = so_cov.selectblock(cov, modes, n_bins = n_bins, block = m1 + m2)
+                cov_dict[(ms1, ms2, m1), (ms3, ms4, m2)] = cov
 
-    results_dict[array] = {"multipole_range": multipole_range[array],
-                           "ref_array": ref_arrays[array],
-                           "calibs": [cal_mean, cal_std]}
+        # Concatenation
+        spec_vec, full_cov = consistency.append_spectra_and_cov(ps_dict, cov_dict, spectra_for_cal)
 
-    calib_vec = np.array([cal_mean**2, cal_mean, 1])
-    res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov,
-                                                          proj_pattern,
-                                                          calib_vec = calib_vec)
-    np.savetxt(f"{residual_output_dir}/residual_{array}_after.dat", np.array([lb, res_spectrum]).T)
-    consistency.plot_residual(lb, res_spectrum, {"analytical": res_cov}, "TT", array,
-                              f"{residual_output_dir}/residual_{array}_after",
-                              lrange=id, l_pow=1)
+        # Save and plot residuals before calibration
+        res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov, proj_pattern)
+        np.savetxt(f"{residual_output_dir}/residual_{name}_before.dat", np.array([lb, res_spectrum]).T)
+        np.savetxt(f"{residual_output_dir}/residual_cov_{name}.dat", res_cov)
+
+        lmin, lmax = multipole_range[map_set]
+        id = np.where((lb >= lmin) & (lb <= lmax))[0]
+        consistency.plot_residual(lb, res_spectrum, {"analytical": res_cov}, "TT", f"{map_set} {test}",
+                                  f"{plot_output_dir}/residual_{name}_before",
+                                  lrange=id, l_pow=1, ylims=y_lims["TT"])
+
+        # Calibrate the spectra
+        cal_mean, cal_std = consistency.get_calibration_amplitudes(spec_vec, full_cov,
+                                                                   proj_pattern, "TT", id,
+                                                                   f"{chains_dir}/{name}")
+
+
+        results_dict[name] = {"multipole_range": multipole_range[map_set],
+                               "ref_map_set": ref_map_set,
+                               "calibs": [cal_mean, cal_std]}
+
+        calib_vec = np.array([cal_mean**2, cal_mean, 1])
+        res_spectrum, res_cov = consistency.project_spectra_vec_and_cov(spec_vec, full_cov,
+                                                                        proj_pattern,
+                                                                        calib_vec = calib_vec)
+  
+        np.savetxt(f"{residual_output_dir}/residual_{name}_after.dat", np.array([lb, res_spectrum]).T)
+        consistency.plot_residual(lb, res_spectrum, {"analytical": res_cov}, "TT", f"{map_set} {test}",
+                                  f"{plot_output_dir}/residual_{name}_after",
+                                  lrange=id, l_pow=1, ylims=y_lims["TT"])
+
+
+
+# plot the cal factors
+color_list =  ["blue", "red", "green"]
+
+for i, ar in enumerate(d["arrays_dr6"]):
+    map_set = f"dr6_{ar}"
+    ref_map_set = ref_map_sets[map_set]
+    print(f"**************")
+    print(f"calibration {map_set} with {ref_map_set}")
+
+    for j, test in enumerate(tests):
+        name, _ = get_proj_pattern(test, map_set, ref_map_set)
+        cal, std = results_dict[name]["calibs"]
+        print(f"{test}, cal: {cal}, sigma cal: {std}")
+
+        plt.errorbar(i + 0.9 + j * 0.1, cal, std, label = test,
+                     color = color_list[j], marker = ".",
+                     ls = "None",
+                     markersize=6.5,
+                     markeredgewidth=2)
+
+    if i == 0:
+        plt.legend(fontsize = 15)
+
+x = np.arange(1,5)
+plt.xticks(x, d["arrays_dr6"])
+plt.ylim(0.967, 1.06)
+plt.tight_layout()
+plt.savefig("{plot_output_dir}calibs_summary.pdf", bbox_inches="tight")
+plt.clf()
+plt.close()
 
 pickle.dump(results_dict, open(f"{output_dir}/calibs_dict.pkl", "wb"))

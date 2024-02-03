f67add7d4baf1afa0346e46e906da5c2d943d7d7
Thibaut Louis <thbtlouis@gmail.com>
2024-02-03 14:02:54 +0100

    add an option for the ng sim

--- project/data_analysis/python/port2sacc.py
+++ project/data_analysis/python/port2sacc.py
@@ -20,48 +20,36 @@ d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
 log = log.get_logger(**d)
 
-# Either use simulation or real data
-store_data = True
+# choose which type of sacc you to create: data_sacc, ng_sim_sacc, simu_sacc
+sacc_fname = "ng_simu_sacc"
 
 # Set covariance file name
-cov_name = "analytic_cov"
-
 mcm_dir = cvt.get_mcms_dir()
 cov_dir = cvt.get_covariances_dir()
-spec_dir = cvt.get_spectra_dir() if store_data else cvt.get_sim_spectra_dir()
 sacc_dir = cvt.get_sacc_dir(create=True)
 
 spec_name_list, nu_tag_list = pspipe_list.get_spec_name_list(d, delimiter="_", return_nu_tag=True)
-log.info(f"Spectra name list: {spec_name_list}")
 
 # Get spectra/cov orders
 spectra_order = cvt.get_spectra_order()
+
 cov_order = pspipe_list.x_ar_cov_order(spec_name_list, nu_tag_list, spectra_order=spectra_order)
 
-# Reading covariance
-log.info(f"Getting '{cov_name}' covariance...")
-cov = np.load(os.path.join(cov_dir, f"x_ar_{cov_name}.npy"))
-
-# Build list of survey x array
-svxar = pspipe_list.get_map_set_list(d)
-
-# Reading beams
-beams = {
-    f"{sv_ar}": misc.read_beams(d[f"beam_T_{sv_ar}"], d[f"beam_pol_{sv_ar}"]) for sv_ar in svxar
-}
-log.debug(f"Beam information: {beams}")
+# Build list of the different map_set
+map_set_list = pspipe_list.get_map_set_list(d)
 
 # Reading passbands : the passband file should be within the dict file
 passbands = {}
-for sv_ar in svxar:
-    freq_info = d[f"freq_info_{sv_ar}"]
+for map_set in map_set_list:
+    freq_info = d[f"freq_info_{map_set}"]
     if d["do_bandpass_integration"]:
         nu_ghz, passband = np.loadtxt(freq_info["passband"]).T
     else:
         nu_ghz, passband = np.array([freq_info["freq_tag"]]), np.array([1.0])
 
-    passbands[f"{sv_ar}"] = [nu_ghz, passband]
-log.debug(f"Passband information: {passbands}")
+    passbands[f"{map_set}"] = [nu_ghz, passband]
+
+log.debug(f"Passband information: {passbands} \n")
 
 # Get Bbl : same bbl for every cross so load one and then assign it to every cross (to make the code
 # as versatile as possible)
@@ -96,38 +84,24 @@ common_kwargs = dict(
     lmax=d["lmax"],
     cov_order=cov_order,
     passbands=passbands,
-    beams=beams,
     metadata=metadata,
     log=log,
 )
 
-# Store data with sacc
-if store_data:
-    # Reading the flat data vector
-    data_vec = covariance.read_x_ar_spectra_vec(
-        spec_dir, spec_name_list, "cross", spectra_order=spectra_order, type=d["type"]
-    )
 
-    sacc_file_name = os.path.join(sacc_dir, f"{'_'.join(d['surveys'])}_data_sacc.fits")
-    io.port2sacc(
-        **common_kwargs, data_vec=data_vec, cov=cov, bbls=bbls, sacc_file_name=sacc_file_name
-    )
+if sacc_fname == "simu_sacc":
 
-else:
-    chi2_list = []
-    # Get theory vec for chi2 check
-    theory_vec = covariance.read_x_ar_theory_vec(
-        cvt.get_best_fits_dir(), mcm_dir, spec_name_list, d["lmax"], spectra_order=spectra_order
-    )
-    # Inverting covariance
-    log.info(f"Inverting '{cov_name}' covariance...")
-    inv_cov = np.linalg.inv(cov)
+    spec_dir = "sim_spectra"
+    cov_name = "final_cov_sim"
+
+    cov = np.load(os.path.join(cov_dir, f"x_ar_{cov_name}.npy"))
+
 
     i_start = d["iStart"]
     i_stop = d["iStop"] + 1
     log.info(f"Storing simulations from {i_start} to {i_stop}...")
-    for i in range(i_start, i_stop):
-        log.info(f"Storing simulation n°{i}...")
+    for iii in range(i_start, i_stop):
+        log.info(f"Storing simulation n°{iii}...")
 
         # Reading the flat data vector
         data_vec = covariance.read_x_ar_spectra_vec(
@@ -135,7 +109,7 @@ else:
         )
 
         # Let's store covariance and bbl in one extra file
-        if i == i_start:
+        if iii == i_start:
             io.port2sacc(
                 **common_kwargs,
                 data_vec=data_vec,
@@ -144,26 +118,39 @@ else:
                 sacc_file_name=os.path.join(sacc_dir, f"{cov_name}_and_Bbl.fits"),
             )
 
-        sacc_file_name = os.path.join(sacc_dir, f"{'_'.join(d['surveys'])}_simu_sacc_{i:05d}.fits")
+        sacc_file_name = os.path.join(sacc_dir, f"{'_'.join(d['surveys'])}_{sacc_fname}_{iii:05d}.fits")
         io.port2sacc(
             **common_kwargs, data_vec=data_vec, cov=None, bbls=None, sacc_file_name=sacc_file_name
         )
 
-        res = data_vec - theory_vec
-        chi2_list.append(chi2 := res @ inv_cov @ res)
-        log.info(f"Check X²/DoF = {chi2:.2f}/{len(data_vec)}")
+else:
 
-    import scipy.stats as ss
+    if sacc_fname == "data_sacc":
+    
+        spec_dir = "spectra_leak_corr_ab_corr"
+        cov_name = "final_cov_data"
+        
+    if sacc_fname == "ng_simu_sacc":
+    
+        spec_dir = "sim_spectra_ng"
+        cov_name = "final_cov_sim"
 
-    x = np.linspace(len(res) * 0.8, len(res) * 1.2, 300)
-    kernel = ss.gaussian_kde(chi2_list)
-    y_kde = kernel(x)
-    y_pdf = ss.chi2(len(res)).pdf(x)
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.set(xlabel=r"$\chi^2$", yticks=[])
-    ax.hist(chi2_list, density=True, bins=15, color="blue", alpha=0.4)
-    ax.plot(x, y_kde, color="darkorange", label="KDE")
-    ax.plot(x, y_pdf, color="forestgreen", label=r"$\chi^2$ dist.")
-    ax.legend()
-    fig.tight_layout()
-    fig.savefig(os.path.join(cvt.get_plots_dir(), "chi2_sacc.png"), dpi=300)
+    log.info(f"Getting '{cov_name}' covariance...")
+    cov = np.load(os.path.join(cov_dir, f"x_ar_{cov_name}.npy"))
+
+    # Reading the flat data vector
+    data_vec = covariance.read_x_ar_spectra_vec(
+        spec_dir, spec_name_list, "cross", spectra_order=spectra_order, type=d["type"]
+    )
+
+    sacc_file_name = os.path.join(sacc_dir, f"{'_'.join(d['surveys'])}_{sacc_fname}.fits")
+    io.port2sacc(
+        **common_kwargs, data_vec=data_vec, cov=cov, bbls=bbls, sacc_file_name=sacc_file_name
+    )
+
+
+log.info(f"Spectra name list: {spec_name_list} \n")
+log.info(f"spectra_order '{spectra_order}' \n")
+log.info(f"WE HAVE WRITTEN THE SACC FILE IDENTIFIED BY {sacc_fname.upper()} \n ")
+log.info(f"WE USED THE '{cov_name.upper()}' COVARIANCE \n")
+log.info(f"AND THE SPECTRA LOCATED IN '{spec_dir.upper()}'  \n")

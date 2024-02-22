74d35c93bcb794bc328a6814f0809f9a566dcc8c
Thibaut Louis <thbtlouis@gmail.com>
2024-02-22 15:24:14 +0100

    include code to compute non gaussianty of poisson sources

--- project/data_analysis/python/get_combined_cov_mats.py
+++ project/data_analysis/python/get_combined_cov_mats.py
@@ -35,19 +35,19 @@ x_ar_analytic_cov = np.load(f"{cov_dir}/x_ar_analytic_cov.npy")
 x_ar_mc_cov = np.load(f"{cov_dir}/x_ar_mc_cov.npy")
 x_ar_beam_cov = np.load(f"{cov_dir}/x_ar_beam_cov.npy")
 x_ar_leakage_cov = np.load(f"{cov_dir}/x_ar_leakage_cov.npy")
+x_ar_non_gaussian_cov_radio = np.load(f"{cov_dir}/x_ar_non_gaussian_cov_radio.npy")
 
 x_ar_cov =  covariance.correct_analytical_cov_skew(x_ar_analytic_cov, x_ar_mc_cov, nkeep=nkeep)
 
 np.save(f"{cov_dir}/x_ar_final_cov_sim.npy", x_ar_cov)
 
-full_x_ar_cov = x_ar_cov + x_ar_beam_cov + x_ar_leakage_cov
+full_x_ar_cov = x_ar_cov + x_ar_beam_cov + x_ar_leakage_cov + x_ar_non_gaussian_cov_radio
 
 np.save(f"{cov_dir}/x_ar_final_cov_data.npy", full_x_ar_cov)
 
-
 ### plot and test covariance in the used ell range
 
-selected_spectra = spectra
+selected_spectra = "TT"
 spectra_cuts = {
     "dr6_pa4_f220": dict(T=[975, lmax], P=[lmax, lmax]),
     "dr6_pa5_f150": dict(T=[775, lmax], P=[475, lmax]),
@@ -56,13 +56,17 @@ spectra_cuts = {
     "dr6_pa6_f090": dict(T=[975, lmax], P=[475, lmax]),
 }
 
+only_TT_map_set = ["dr6_pa4_f220"]
+
+
 bin_out_dict,  all_indices = covariance.get_indices(bin_low,
                                                     bin_high,
                                                     bin_mean,
                                                     spec_name_list,
                                                     spectra_cuts=spectra_cuts,
                                                     spectra_order=spectra,
-                                                    selected_spectra=selected_spectra)
+                                                    selected_spectra=selected_spectra,
+                                                    only_TT_map_set=only_TT_map_set)
 
 
 sub_full_x_ar_cov = full_x_ar_cov[np.ix_(all_indices, all_indices)]
@@ -70,6 +74,7 @@ sub_x_ar_cov = x_ar_cov[np.ix_(all_indices, all_indices)]
 sub_x_ar_analytic_cov = x_ar_analytic_cov[np.ix_(all_indices, all_indices)]
 sub_x_ar_beam_cov = x_ar_beam_cov[np.ix_(all_indices, all_indices)]
 sub_x_ar_leakage_cov = x_ar_leakage_cov[np.ix_(all_indices, all_indices)]
+sub_x_ar_non_gaussian_cov_radio = x_ar_non_gaussian_cov_radio[np.ix_(all_indices, all_indices)]
 
 
 log.info(f"test S+N cov + beam cov + leakage cov")
@@ -77,24 +82,38 @@ log.info(f"test S+N cov + beam cov + leakage cov")
 pspy_utils.is_symmetric(sub_full_x_ar_cov, tol=1e-7)
 pspy_utils.is_pos_def(sub_full_x_ar_cov)
 
-
 plt.figure(figsize=(28,8))
 
 plt.title(r"$\Sigma^{\rm MC}/\Sigma^{\rm analytic}$", fontsize=22)
 
-xlabel = []
-xlabel_loc = []
+name_list, label_loc = [], []
 for my_spec in bin_out_dict.keys():
     id_spec, lb_spec = bin_out_dict[my_spec]
     mean_id = np.mean(id_spec)
-    xlabel_loc += [mean_id]
-    xlabel += [my_spec]
+    label_loc += [mean_id]
+    
+    name, spectrum = my_spec
+    name = name.replace("dr6_", "")
+    name_list += [f"{spectrum} {name}"]
+
     plt.axvline(np.max(id_spec) + 0.5, color="black", alpha=0.4, linestyle="--")
     
 plt.plot(sub_x_ar_cov.diagonal() / sub_x_ar_analytic_cov.diagonal())
-plt.xticks(xlabel_loc, xlabel, rotation=90)
+plt.xticks(label_loc, name_list, rotation=90)
 plt.tight_layout()
 plt.savefig(f"{plot_dir}/MC_vs_analytic_cov.png")
+plt.clf()
+plt.close()
+
+
+corr = so_cov.cov2corr(sub_full_x_ar_cov, remove_diag=False)
+plt.figure(figsize=(18,12))
+plt.imshow(corr)
+plt.colorbar()
+plt.xticks(label_loc, name_list, rotation=90)
+plt.yticks(label_loc, name_list)
+plt.tight_layout()
+plt.savefig(f"{plot_dir}/corr.png")
 plt.clf()
 plt.close()
 
@@ -108,7 +127,8 @@ for my_spec in bin_out_dict.keys():
 plt.title(r"$\Sigma^{\rm comp}/\Sigma^{\rm tot}$", fontsize=22)
 plt.plot(sub_x_ar_leakage_cov.diagonal()/sub_full_x_ar_cov.diagonal(), label="leakage cov / total cov")
 plt.plot(sub_x_ar_beam_cov.diagonal()/sub_full_x_ar_cov.diagonal(), label="beam cov / total cov")
-plt.xticks(xlabel_loc, xlabel, rotation=90)
+plt.plot(sub_x_ar_non_gaussian_cov_radio.diagonal()/sub_full_x_ar_cov.diagonal(), label="non gaussian radio cov / total cov")
+plt.xticks(label_loc, name_list, rotation=90)
 plt.legend(fontsize=18)
 plt.tight_layout()
 plt.savefig(f"{plot_dir}/cov_different_component.png")

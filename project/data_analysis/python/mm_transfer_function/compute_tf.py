266d27740e5853cd6133742a5131f97b94b36f60
Louis Thibaut <louisthibaut@mbp-de-louis.home>
2024-10-15 13:58:57 +0200

    clean up

--- project/data_analysis/python/mm_transfer_function/compute_tf.py
+++ project/data_analysis/python/mm_transfer_function/compute_tf.py
@@ -19,19 +19,6 @@ modes = ["TT", "TE", "ET", "EE"] if d["cov_T_E_only"] else spectra
 
 sv = "dr6"
 arrays = [f"{sv}_{ar}" for ar in d[f"arrays_{sv}"]]
-
-#combin = "AxA_AxP"
-combin = "AxP_PxP"
-
-subtract_bf_fg = True
-
-output_dir = f"tf_estimator_{combin}"
-if subtract_bf_fg:
-    output_dir += "_fg_sub"
-
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
 binning_file = d["binning_file"]
 lmax = d["lmax"]
 
@@ -42,48 +29,63 @@ ref_ars = {"dr6_pa4_f150": "Planck_f143",
            "dr6_pa6_f090": "Planck_f143",
            "dr6_pa6_f150": "Planck_f143"}
 
+subtract_bf_fg = True
+montecarlo_errors = True
 
-lb_list = []
-tf_list = []
-tf_err_list = []
+combins = ["AxA_AxP", "AxP_PxP"]
 
-for i, ar in enumerate(arrays):
+for combin in combins:
 
-    ref_ar = ref_ars[ar]
-
-    if combin == "AxA_AxP":
-        ar1A, ar2A = ar, ar
-        ar1B, ar2B = ar, ref_ar
-        op = "aa/ab"
-    elif combin == "AxP_PxP":
-        ar1A, ar2A = ar, ref_ar
-        ar1B, ar2B = ref_ar, ref_ar
-        op = "ab/bb"
-
-    ar_list = [ar, ref_ar]
-    ps_template = spec_dir + "/Dl_{}x{}_cross.dat"
-    cov_template = f"{cov_dir}/analytic_cov" + "_{}x{}_{}x{}.npy"
-    ps_dict, cov_dict = consistency.get_ps_and_cov_dict(ar_list, ps_template, cov_template)
- 
+    output_dir = f"tf_estimator_{combin}"
     if subtract_bf_fg:
-        for ms1, ms2, spec in ps_dict.keys():
-            if spec != "TT":
-                continue
-            log.info(f"remove fg {spec}  {ms1} x {ms2}")
-            l_fg, bf_fg = so_spectra.read_ps(f"{bestfir_dir}/fg_{ms1}x{ms2}.dat", spectra=spectra)
-            _, bf_fg_TT_binned = pspy_utils.naive_binning(l_fg, bf_fg["TT"], d["binning_file"], d["lmax"])
-            ps_dict[ms1, ms2, spec] -= bf_fg_TT_binned
+        output_dir += "_fg_sub"
 
-    lb, tf, tf_cov, _, _ = consistency.compare_spectra(ar_list, op, ps_dict, cov_dict, mode = "TT")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    tf_list.append(tf)
-    tf_err_list.append(np.sqrt(tf_cov.diagonal()))
-    lb_list.append(lb)
+    lb_list, tf_list, tf_err_list = [], [], []
+    
+    for i, ar in enumerate(arrays):
+
+        ref_ar = ref_ars[ar]
+
+        if combin == "AxA_AxP":
+            ar1A, ar2A = ar, ar
+            ar1B, ar2B = ar, ref_ar
+            op = "aa/ab"
+        elif combin == "AxP_PxP":
+            ar1A, ar2A = ar, ref_ar
+            ar1B, ar2B = ref_ar, ref_ar
+            op = "ab/bb"
+
+        ar_list = [ar, ref_ar]
+        ps_template = spec_dir + "/Dl_{}x{}_cross.dat"
+        
+        if montecarlo_errors == True:
+            cov_template = f"{cov_dir}/mc_cov" + "_{}x{}_{}x{}.npy"
+        else:
+            cov_template = f"{cov_dir}/analytic_cov" + "_{}x{}_{}x{}.npy"
+
+        ps_dict, cov_dict = consistency.get_ps_and_cov_dict(ar_list, ps_template, cov_template)
+ 
+        if subtract_bf_fg:
+            for ms1, ms2, spec in ps_dict.keys():
+                if spec != "TT":
+                    continue
+                log.info(f"remove fg {spec}  {ms1} x {ms2}")
+                l_fg, bf_fg = so_spectra.read_ps(f"{bestfir_dir}/fg_{ms1}x{ms2}.dat", spectra=spectra)
+                _, bf_fg_TT_binned = pspy_utils.naive_binning(l_fg, bf_fg["TT"], d["binning_file"], d["lmax"])
+                ps_dict[ms1, ms2, spec] -= bf_fg_TT_binned
+
+        lb, tf, tf_cov, _, _ = consistency.compare_spectra(ar_list, op, ps_dict, cov_dict, mode = "TT")
+
+        tf_list.append(tf)
+        tf_err_list.append(np.sqrt(tf_cov.diagonal()))
+        lb_list.append(lb)
 
 
-    np.savetxt(f"{output_dir}/tf_estimator_{ar}.dat", np.array([lb, tf, np.sqrt(tf_cov.diagonal())]).T)
-    np.savetxt(f"{output_dir}/tf_cov_{ar}.dat", tf_cov)
+        np.savetxt(f"{output_dir}/tf_estimator_{ar}.dat", np.array([lb, tf, np.sqrt(tf_cov.diagonal())]).T)
+        np.savetxt(f"{output_dir}/tf_cov_{ar}.dat", tf_cov)
 
 
-tf_tools.plot_tf(lb_list, tf_list, tf_err_list,
-                 arrays, f"{output_dir}/tf_estimation.png")
+    tf_tools.plot_tf(lb_list, tf_list, tf_err_list, arrays, f"{output_dir}/tf_estimation.png")
--- project/data_analysis/python/mm_transfer_function/plot_all_tf.py
+++ project/data_analysis/python/mm_transfer_function/plot_all_tf.py
@@ -1,38 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from pspy import so_dict, pspy_utils
+import sys
 
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+
+planck_version = d["planck_version"]
 matplotlib.rcParams["font.family"] = "serif"
 matplotlib.rcParams["font.size"] = "18"
 
+plot_dir = f"plots/tf_{planck_version}"
+pspy_utils.create_directory(plot_dir)
 
-tf_dir = "tf_estimator_AxP_PxP_fg_sub"
+combins = ["AxA_AxP", "AxP_PxP"]
 
-_, end = tf_dir.split('estimator_')
-n1, n2, n3, n4 = end[0], end[2],  end[4], end[6]
+for combin in combins:
 
+    tf_dir = f"tf_estimator_{combin}_fg_sub"
 
-plt.figure(figsize=(16, 8))
+    plt.figure(figsize=(16, 8))
+    colors = ["red", "orange", "green", "blue", "gray"]
+    arrays = ["pa5_f090", "pa6_f090", "pa5_f150", "pa6_f150", "pa4_f220"]
 
-colors = ["red", "orange", "green", "blue", "gray"]
-arrays = ["pa5_f090", "pa6_f090", "pa5_f150", "pa6_f150", "pa4_f220"]
-
-
-count = 0
-for col, ar in zip(colors, arrays):
-    lb, tf, tferr = np.loadtxt(f"{tf_dir}/tf_estimator_dr6_{ar}.dat", unpack=True)
-  #  lth, tf_fit = np.loadtxt(f"{tf_dir}/tf_fit_dr6_{ar}_logistic_fixed_amp.dat", unpack=True)
-
-    plt.errorbar(lb-10 +count*5, tf, tferr, fmt="o", color=col, label=ar) #, color="blue")
-    plt.plot(lb, lb*0+1, "--", color="black", alpha=0.5)#, color="blue")
-    count += 1
+    count = 0
+    for col, ar in zip(colors, arrays):
+        lb, tf, tferr = np.loadtxt(f"{tf_dir}/tf_estimator_dr6_{ar}.dat", unpack=True)
+        plt.errorbar(lb-10 +count*5, tf, tferr, fmt="o", color=col, label=ar)
+        plt.plot(lb, lb*0+1, "--", color="black", alpha=0.5)
+        count += 1
     
-plt.ylabel(r"$T_{\ell} = C_{\ell, \rm %s  x  %s}^{\rm TT} / C_{\ell, \rm %s  x %s}^{\rm TT}$" % (n1, n2, n3, n4), fontsize=25)
-plt.xlabel(r"$\ell$", fontsize=25)
-
-plt.xlim(0, 1800)
-plt.ylim(0.9,1.05)
-plt.legend()
-plt.savefig(f"{tf_dir}.png", bbox_inches="tight")
-plt.clf()
-plt.close()
+    na, nb = combin.split("_")
+    plt.ylabel(r"$T_{\ell} = C_{\ell, \rm %s}^{\rm TT} / C_{\ell, \rm %s}^{\rm TT}$" % (na, nb), fontsize=25)
+    plt.xlabel(r"$\ell$", fontsize=25)
+    plt.xlim(0, 1800)
+    plt.ylim(0.9,1.05)
+    plt.legend()
+    plt.savefig(f"{plot_dir}/{tf_dir}.png", bbox_inches="tight")
+    plt.clf()
+    plt.close()

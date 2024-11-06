f5ce8216e47d797a71b5e0993e7464a9ef33ef17
Louis Thibaut <louisthibaut@client-172-18-116-84.eduroam.universite-paris-saclay.fr>
2024-11-06 13:11:14 +0100

    backup null test

--- project/data_analysis/python/null_tests/custom_nulls/isotropy/check_north_south.py
+++ project/data_analysis/python/null_tests/custom_nulls/isotropy/check_north_south.py
@@ -7,6 +7,16 @@ import sys
 import numpy as np
 import scipy.stats as ss
 import os
+from matplotlib import rcParams
+
+
+rcParams["font.family"] = "serif"
+rcParams["font.size"] = "40"
+rcParams["xtick.labelsize"] = 16
+rcParams["ytick.labelsize"] = 16
+rcParams["axes.labelsize"] = 16
+rcParams["axes.titlesize"] = 16
+
 
 def pte_histo(pte_list, file_name, n_bins):
     n_samples = len(pte_list)
@@ -19,12 +29,14 @@ def pte_histo(pte_list, file_name, n_bins):
     nPTE_low =  len(id_low[0])
     
     plt.figure(figsize=(8,6))
-    plt.xlabel(r"Probability to exceed (PTE)")
-    plt.hist(pte_list, bins=bins, label=f"n tests: {n_samples}, min: {min:.3f}, max: {max:.3f}, [PTE>0.99]: {nPTE_high},  [PTE<0.01]: {nPTE_low} ")
-    plt.axhline(n_samples/n_bins, color="k", ls="--")
+    plt.title("Isotropy test: North/South", fontsize=16)
+    plt.xlabel(r"Probability to exceed (PTE)", fontsize=16)
+    plt.hist(pte_list, bins=bins, label=f"n tests: {n_samples}, min: {min:.3f}, max: {max:.3f}", histtype='bar', facecolor="orange", edgecolor="black", linewidth=3)
+    plt.axhline(n_samples/n_bins, color="k", ls="--", alpha=0.5)
+    plt.xlim(0, 1)
     plt.tight_layout()
-    plt.legend()
-    plt.savefig(f"{file_name}", dpi=300)
+    plt.legend(fontsize=16)
+    plt.savefig(f"{file_name}", dpi=300, bbox_inches='tight')
     plt.clf()
     plt.close()
 
@@ -57,17 +69,18 @@ lmin["dr6_pa5_f150", "ET"] = 800
 lmin["dr6_pa6_f090", "ET"] = 1000
 lmin["dr6_pa6_f150", "ET"] = 600
 
-lmin["dr6_pa4_f220", "EE"] = 500
-lmin["dr6_pa5_f090", "EE"] = 500
-lmin["dr6_pa5_f150", "EE"] = 500
-lmin["dr6_pa6_f090", "EE"] = 500
-lmin["dr6_pa6_f150", "EE"] = 500
+lmin["dr6_pa4_f220", "EE"] = 1000
+lmin["dr6_pa5_f090", "EE"] = 1000
+lmin["dr6_pa5_f150", "EE"] = 800
+lmin["dr6_pa6_f090", "EE"] = 1000
+lmin["dr6_pa6_f150", "EE"] = 600
 
-lmin["dr6_pa4_f220", "BB"] = 500
-lmin["dr6_pa5_f090", "BB"] = 500
-lmin["dr6_pa5_f150", "BB"] = 500
-lmin["dr6_pa6_f090", "BB"] = 500
-lmin["dr6_pa6_f150", "BB"] = 500
+
+lmin["dr6_pa4_f220", "BB"] = 1000
+lmin["dr6_pa5_f090", "BB"] = 1000
+lmin["dr6_pa5_f150", "BB"] = 800
+lmin["dr6_pa6_f090", "BB"] = 1000
+lmin["dr6_pa6_f150", "BB"] = 600
 
 lmin["dr6_pa4_f220", "TB"] = 1000
 lmin["dr6_pa5_f090", "TB"] = 1000
@@ -81,22 +94,25 @@ lmin["dr6_pa5_f150", "BT"] = 800
 lmin["dr6_pa6_f090", "BT"] = 1000
 lmin["dr6_pa6_f150", "BT"] = 600
 
-lmin["dr6_pa4_f220", "EB"] = 500
-lmin["dr6_pa5_f090", "EB"] = 500
-lmin["dr6_pa5_f150", "EB"] = 500
-lmin["dr6_pa6_f090", "EB"] = 500
-lmin["dr6_pa6_f150", "EB"] = 500
+lmin["dr6_pa4_f220", "EB"] = 1000
+lmin["dr6_pa5_f090", "EB"] = 1000
+lmin["dr6_pa5_f150", "EB"] = 800
+lmin["dr6_pa6_f090", "EB"] = 1000
+lmin["dr6_pa6_f150", "EB"] = 600
 
-lmin["dr6_pa4_f220", "BE"] = 500
-lmin["dr6_pa5_f090", "BE"] = 500
-lmin["dr6_pa5_f150", "BE"] = 500
-lmin["dr6_pa6_f090", "BE"] = 500
-lmin["dr6_pa6_f150", "BE"] = 500
+lmin["dr6_pa4_f220", "BE"] = 1000
+lmin["dr6_pa5_f090", "BE"] = 1000
+lmin["dr6_pa5_f150", "BE"] = 800
+lmin["dr6_pa6_f090", "BE"] = 1000
+lmin["dr6_pa6_f150", "BE"] = 600
+
+
+
 
 null_dir = "isotropy"
 pspy_utils.create_directory(null_dir)
 
-nsims = 320
+nsims = 300
 
 my_ylim = {}
 my_ylim["TT"] = [-200,200]
@@ -127,7 +143,7 @@ g.write('<body> \n')
 g.write('<h1>isotropy </h1>')
 g.write('<p> In this webpage we host all plots for isotropy  tests, we have divided the observed area into two regions of the sky </p>')
 g.write('<p> we have done N tests corresponding to the following  PTE distribution </p>')
-g.write('<img src="' + 'isotropy/all_pte.png' + '" width="50%" /> \n')
+g.write('<img src="' + 'isotropy/pte_isotropy.png' + '" width="50%" /> \n')
 g.write('<p> you can have a look at all spectra, press a/z to change the null, c/v to change spectrum (TT,TE, .. BB) </p>')
 g.write('<div class=null> \n')
 
@@ -148,6 +164,7 @@ for my_spec in spectra:
         lb, bf_north_b = pspy_utils.naive_binning(lth, bf_north[my_spec], binning_file, lmax)
         lb, bf_south_b = pspy_utils.naive_binning(lth, bf_south[my_spec], binning_file, lmax)
 
+        n_bins = len(lb)
         na, nb = spec.split("x")
         my_lmin = np.maximum(lmin[na, my_spec], lmin[nb, my_spec])
 
@@ -161,12 +178,32 @@ for my_spec in spectra:
             lb, Db_north_sim = so_spectra.read_ps(f"patch_north/sim_spectra/Dl_{spec}_cross_{iii:05d}.dat", spectra=spectra)
             lb, Db_south_sim = so_spectra.read_ps(f"patch_south/sim_spectra/Dl_{spec}_cross_{iii:05d}.dat", spectra=spectra)
             diff_all += [Db_north_sim[my_spec] - Db_south_sim[my_spec]]
+            
         diff_mean = np.mean(diff_all, axis=0)
         diff_std = np.std(diff_all, axis=0)
+        diff_cov_mc = np.cov(diff_all, rowvar=False)
 
         id = np.where(lb > my_lmin)
         
-        chi2 = np.sum((diff[id]- diff_bf[id]) ** 2 / diff_std[id] ** 2)
+        cov_north = np.load(f"patch_north/covariances/analytic_cov_{spec}_{spec}.npy")
+        cov_south = np.load(f"patch_south/covariances/analytic_cov_{spec}_{spec}.npy")
+
+        diff_cov = cov_north + cov_south
+        diff_cov = so_cov.selectblock(diff_cov, spectra, n_bins, block=my_spec+my_spec)
+        
+        an_var = diff_cov.diagonal()
+        mc_var = diff_cov_mc.diagonal()
+
+        print(my_spec, spec)
+        diff_cov_mc = diff_cov - np.diag(an_var) + np.diag(mc_var)
+
+        diff_cov_mc = diff_cov_mc[np.ix_(id[0], id[0])]
+        
+        chi2 = (diff[id] - diff_bf[id]) @ np.linalg.inv(diff_cov_mc) @ (diff[id] - diff_bf[id])
+        chi2_diag = np.sum((diff[id] - diff_bf[id]) ** 2 / diff_std[id] ** 2)
+        print(my_spec, spec, "diag",chi2_diag, "full", chi2)
+        
+        
         ndof = len(lb[id])
         pte = 1 - ss.chi2(ndof).cdf(chi2)
         
@@ -219,9 +256,9 @@ for my_spec in spectra:
     g.write('</div>\n')
 
 
-n_bins = 10
+n_bins = 14
 print(np.min(pte_list), np.max(pte_list))
-pte_histo(pte_list, f"{null_dir}/all_pte.png", n_bins)
+pte_histo(pte_list, f"{null_dir}/pte_isotropy_all.png", n_bins)
 
 g.write('</div> \n')
 g.write('</body> \n')

8d07e89fc4bf0329751c746dac8cc976cf1fa779
Louis Thibaut <louisthibaut@mbp-de-louis.home>
2025-01-29 20:12:06 +0100

    change plotting style

--- project/ACT_DR6/python/paper_plots/results_multifrequency_TT.py
+++ project/ACT_DR6/python/paper_plots/results_multifrequency_TT.py
@@ -40,40 +40,67 @@ spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
 if show_220 == True:
     case_list = ["150x150", "90x90", "90x150", "90x220", "150x220", "220x220"]
 else:
-    case_list = ["90x90", "150x150", "90x150", "90x220", "150x220"]
+    case_list = [ "90x90", "90x150", "150x150", "90x220", "150x220"]
 
 ########################################################################################
 
 lth, Dlth = so_spectra.read_ps(f"{bestfit_dir}/cmb.dat", spectra=spectra)
 
 
-color_list = ["green", "red", "blue", "purple", "gray", "cyan"]
-
-count = 0
+color_list = ["green", "red", "royalblue", "orange", "purple"]
 
 
-plt.figure(figsize=(16, 12))
-plt.semilogy()
-plt.ylabel(r"$D^{TT}_{\ell} [\mu K^{2}] $", fontsize=40)
+
+lpow = 0.1
+multipole = np.array([ 1000, 2000, 3000, 4000, 5000])#, 6000, 7000, 8000])
+ell_tick = multipole ** lpow
+ell_label = multipole
+
+
+plt.figure(figsize=(16, 8))
+#plt.semilogy()
+plt.ylabel(r"$\ell^{2} D^{TT}_{\ell} [m K^{2}] $", fontsize=40)
 plt.xlabel(r"$\ell$", fontsize=40)
 
-plt.plot(lth, Dlth["TT"], color="gray", alpha=0.6, linestyle="--")
+plt.plot(lth ** lpow, Dlth["TT"] * lth **2 / 10**6, color="gray", alpha=0.6, linestyle="--")
 
-plt.ylim(20, 7*10**3)
-plt.xlim(0,8000)
+shift_dict = {}
+shift_dict["90x90"] = -5
+shift_dict["90x150"] = 5
+shift_dict["150x150"] = 0
+shift_dict["90x220"] = -10
+shift_dict["150x220"] = 10
+
+plt.ylim(0.3*10**3, 2*10**3)
+plt.xlim(500 ** lpow, 4500 ** lpow)
+shift=5
+count=0
 for color, case in zip(color_list, case_list):
             
     lb_ml, vec_ml, sigma_ml = np.loadtxt(f"{combined_spec_dir}/{type}_{case}_TT.dat", unpack=True)
     lb_ml, vec_th_ml = np.loadtxt(f"{combined_spec_dir}/bestfit_{case}_TT.dat", unpack=True)
 
-    id = np.where(lb_ml>1500)
+    if "220" in case:
+        print(case, "ok")
+        id = np.where(lb_ml>1500)
+        alpha=0.4
+
+    else:
+        id = np.where(lb_ml>500)
+        alpha=1
     
     fa, fb = case.split("x")
     
-    plt.errorbar(lb_ml, vec_ml, sigma_ml , fmt=".", color=color,  label=f"{fa} GHz x {fb} GHz")
-    plt.errorbar(lb_ml[id], vec_th_ml[id], color=color, fmt="--", alpha=0.7)
+    plt.errorbar((lb_ml[id]+ shift_dict[case]) ** lpow, vec_ml[id] * lb_ml[id] **2 / 10**6, sigma_ml[id] * lb_ml[id] **2 / 10**6, fmt=".", color=color,  label=f"{fa} GHz x {fb} GHz", alpha=alpha, mfc='w')
+    plt.errorbar((lb_ml[id]) ** lpow, vec_th_ml[id] * lb_ml[id]  **2 / 10**6, color=color, fmt="--", alpha=0.7*alpha)
 
-plt.legend(fontsize=24)
+
+    count += 1
+plt.xticks(ticks=ell_tick, labels=ell_label,fontsize=20)
+
+
+plt.legend(fontsize=20, loc=(0.55,0.6))
+#plt.show()
 plt.savefig(f"{paper_plot_dir}/multifrequency_spectra_TT{tag}.pdf", bbox_inches="tight")
 plt.clf()
 plt.close()

5e30304f9a94ca5d59a0ab0191c196af01f1f144
Merry Duparc <duparc@ijclab.in2p3.fr>
2025-12-11 07:30:01 -0800

    Add source_subtract.py

---
+++ project/SO/pISO/python/subtract_sources.py
@@ -0,0 +1,54 @@
+"""Subtract sources from given maps and copy both initial maps and srcfree maps.
+TODO : Make UHF source map
+TODO : get actual source subtracted maps and don't use this script anymore :)
+"""
+import sys
+import time
+import os
+import numpy as np
+from pixell import enmap, enplot
+from pspipe_utils import kspace, log, misc, pspipe_list
+from pspy import pspy_utils, so_dict, so_map, so_mpi, sph_tools
+
+print(sys.argv)
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+log = log.get_logger(**d)
+
+surveys_to_subtract = ['lat_iso']   # Only work with LAT ISO
+d['surveys'] = surveys_to_subtract
+
+n_ar, sv_list, ar_list = pspipe_list.get_arrays_list(d)
+
+log.info(f"number of arrays for the mpi loop : {n_ar}")
+so_mpi.init(True)
+subtasks = so_mpi.taskrange(imin=0, imax=n_ar-1)
+
+act_map_path = d["maps_dir_dr6"]
+# TODO : take the mean across tube & splits
+map_f090 = enmap.read_map(f'{act_map_path}/act_dr6.02_std_AA_night_pa5_f090_4way_set0_map.fits')
+map_f090_srcfree = enmap.read_map(f'{act_map_path}/act_dr6.02_std_AA_night_pa5_f090_4way_set0_map_srcfree.fits')
+map_f150 = enmap.read_map(f'{act_map_path}/act_dr6.02_std_AA_night_pa5_f150_4way_set0_map.fits')
+map_f150_srcfree = enmap.read_map(f'{act_map_path}/act_dr6.02_std_AA_night_pa5_f150_4way_set0_map_srcfree.fits')
+
+srcmap_f090 = map_f090 - map_f090_srcfree
+srcmap_f150 = map_f150 - map_f150_srcfree
+
+for task in subtasks:
+    task = int(task)
+    sv, ar = sv_list[task], ar_list[task]
+    
+    for i, map_fn in enumerate(d[f'maps_{sv}_{ar}']):
+        log.info(f'{sv}_{ar}_split{i}')
+        input_map = enmap.read_map(map_fn)
+        lat_dir = os.path.dirname(map_fn)
+        
+        if 'f090' in map_fn:
+            srcmap = srcmap_f090
+        elif 'f150' in map_fn:
+            srcmap = srcmap_f150
+            
+        srcfree_map = input_map - srcmap
+        enmap.write_map(f'{map_fn[:-5]}_srcfree.fits', srcfree_map)
+        enplot.write(f'{map_fn[:-5]}_srcfree.fits', enplot.plot(srcfree_map, downgrade=8, ticks=1, colorbar=True, range=(300, 100, 100)))
+

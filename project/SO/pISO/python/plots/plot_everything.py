51037ecc680db0726cac51adb61b2990ad8c3820
Merry Duparc <duparc@ijclab.in2p3.fr>
2026-01-08 05:47:51 -0800

    update plot and fit noise scripts

--- project/SO/pISO/python/plots/plot_everything.py
+++ project/SO/pISO/python/plots/plot_everything.py
@@ -64,7 +64,49 @@ spectra_noise_template = spectra_path + "/Dl_{}x{}_noise.dat"
 survey_B = "lat_iso"
 arrays_B = d[f"arrays_{survey_B}"]
 surveys_arrays_B = [f"{survey_B}_{ar}" for ar in arrays_B]
+# Define where and what to plot
 
+save_path = plot_dir + "/spectra/"
+os.makedirs(save_path, exist_ok=True)
+save_path_cross = save_path + "cross/"
+os.makedirs(save_path_cross, exist_ok=True)
+save_path_cross_freqs = save_path + "cross_freqs/"
+os.makedirs(save_path_cross_freqs, exist_ok=True)
+save_path_cross_noises = save_path + "noises/"
+os.makedirs(save_path_cross_noises, exist_ok=True)
+save_path_TF = save_path + "TF/"
+os.makedirs(save_path_TF, exist_ok=True)
+
+clfile = "/global/cfs/cdirs/sobs/users/merrydup/deep56/proposal_plots/cmb.dat"
+l, ps_theory = so_spectra.read_ps(clfile, spectra=spectra)
+lmax = d["lmax"]
+binning_file = d["binning_file"]
+Dlb_theory_dumb = {
+    spec: pspy_utils.naive_binning(
+        l[:lmax],
+        ps_theory[spec][:lmax],
+        binning_file=binning_file,
+        lmax=d["lmax"],
+    )[1]
+    for spec in ["TT", "EE"]
+}
+
+surveys_arrays = [f"{survey}_{ar}" for survey in d['surveys'] for ar in d[f'arrays_{survey}']]
+
+
+Dls_cross = {}
+Dls_noise = {}
+for sv_ar1, sv_ar2 in itertools.combinations_with_replacement(surveys_arrays, r=2):
+    ls, Dls_cross[f"{sv_ar1}x{sv_ar2}"] = so_spectra.read_ps(
+        spectra_cross_template.format(sv_ar1, sv_ar2), spectra=spectra
+    )
+    try:
+        ls, Dls_noise[f"{sv_ar1}x{sv_ar2}"] = so_spectra.read_ps(
+            spectra_noise_template.format(sv_ar1, sv_ar2), spectra=spectra
+        )
+    except:
+        pass
+fac = ls * (ls + 1) / (2 * np.pi)
 beams = {
     sv_ar: pspy_utils.naive_binning(
         np.loadtxt(d[f"beam_T_{sv_ar}"]).T[0],
@@ -76,8 +118,153 @@ beams = {
     for sv_ar in surveys_arrays_B
 }
 
+# BxB cross plots
+for f in spectra:
+    fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
+    ax.plot(l, ps_theory[f], color="black", label="theory")
+
+    for sv_ar1, sv_ar2 in itertools.combinations_with_replacement(
+        surveys_arrays_B, r=2
+    ):
+        ax.plot(ls, Dls_cross[f"{sv_ar1}x{sv_ar2}"][f], label=f"{sv_ar1}x{sv_ar2}")
+
+    ax.set_xlabel(r"$\ell$", fontsize=18)
+    ax.set_ylabel(rf"$D^{{{f}}}_\ell$", fontsize=18)
+    ax.set_title(f)
+    ax.set_yscale(plot_info["yscale"][f])
+    ax.set_ylim(*plot_info["cross_AxB"]["ylims"][f])
+    ax.set_xlim(*plot_info["cross_AxB"]["xlims"][f])
+    ax.legend()
+
+    plt.savefig(save_path_cross + f"cross_{survey_B}x{survey_B}_{f}")
+    plt.close()
+
+# BxB cross plots per frequency
+frequencies = ["090", "150", "220", "280"]
+for freq1, freq2 in itertools.combinations_with_replacement(frequencies, r=2):
+    if True in [freq1 in sv_ar for sv_ar in surveys_arrays_B]:
+        if True in [freq2 in sv_ar for sv_ar in surveys_arrays_B]:
+            for f in spectra:
+                fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
+                ax.plot(
+                    l,
+                    ps_theory[f],
+                    color="black",
+                    label="theory",
+                )
+
+                for sv_ar1, sv_ar2 in itertools.combinations_with_replacement(
+                        surveys_arrays_B, r=2
+                    ):
+                        if (freq1 in sv_ar1) & (freq2 in sv_ar2):
+                            ax.plot(
+                                ls,
+                                Dls_cross[f"{sv_ar1}x{sv_ar2}"][f],
+                                label=f"{sv_ar1}x{sv_ar2}",
+                            )
+
+                ax.set_xlabel(r"$\ell$", fontsize=18)
+                ax.set_ylabel(rf"$D^{{{f}}}_\ell$", fontsize=18)
+                ax.set_title(f)
+                ax.set_yscale(plot_info["yscale"][f])
+                ax.set_ylim(*plot_info["cross_AxB"]["ylims"][f])
+                ax.set_xlim(*plot_info["cross_AxB"]["xlims"][f])
+                ax.legend()
+
+                plt.savefig(
+                    save_path_cross_freqs
+                    + f"cross_{survey_B}x{survey_B}_{f}_{freq1}x{freq2}"
+                )
+                plt.close()
+
+# BxB noise plots
+Nls = {}
+for rms in [5, 10, 15, 20, 25, 30]:
+    ls_nls, Nls[rms] = pspy_utils.get_nlth_dict(
+        rms, type="Cl", lmax=d["lmax"], spectra=spectra
+    )
+
+# Choose a colormap and create a list of colors
+cmap = plt.get_cmap("viridis")  # try 'plasma', 'coolwarm', 'turbo', etc.
+norm = Normalize(vmin=0, vmax=5)
+colors = [cmap(norm(i)) for i in range(6)]
+
+for f in spectra_auto:
+    fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
+    ax.plot(l, ps_theory[f], color="black", label="theory")
+    for sv_ar2 in surveys_arrays_B:
+        ax.plot(
+            ls,
+            Dls_noise[f"{sv_ar2}x{sv_ar2}"][f] / fac * beams[sv_ar2] ** 2,
+            label=f"{sv_ar2}",
+        )
+
+    for i, (rms, nls) in enumerate(Nls.items()):
+        ax.plot(
+            ls_nls, nls[f], label=f"{rms}", color=colors[i], lw=4, alpha=0.4, zorder=-10
+        )
+
+    ax.set_xlabel(r"$\ell$", fontsize=18)
+    ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
+    ax.set_title(f)
+    ax.set_yscale("log")
+    ax.set_ylim(*plot_info["noise_rms_BxB"]["ylims"][f])
+    ax.set_xlim(*plot_info["noise_rms_BxB"]["xlims"][f])
+    ax.legend()
+    plt.savefig(save_path_cross_noises + f"noise_rms_{survey_B}x{survey_B}_{f}")
+    plt.close()
+
+# BxB noise plots
+for f in spectra_auto:
+    for survey in d['surveys']:
+    
+        fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
+        ax.plot(l, ps_theory[f], color="black", label="theory")
+
+        for ar in d[f'arrays_{survey}']:
+            sv_ar2 = f'{survey}_{ar}'
+            ax.plot(ls, Dls_noise[f"{sv_ar2}x{sv_ar2}"][f] / fac, label=f"{sv_ar2}")
+
+        ax.set_xlabel(r"$\ell$", fontsize=18)
+        ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
+        ax.set_title(f)
+        ax.set_yscale("log")
+        ax.set_ylim(*plot_info["noise_BxB"]["ylims"][f])
+        ax.set_xlim(*plot_info["noise_BxB"]["xlims"][f])
+        ax.legend()
+        plt.savefig(save_path_cross_noises + f"noise_{survey}x{survey}_{f}")
+        plt.close()
+
+# BxB correlated noise plots
+for f in spectra:
+    if f != f[::-1]:
+        fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
+        ax.plot(l, ps_theory[f], color="black", label="theory")
+
+        for sv_ar1 in surveys_arrays_B:
+            for sv_ar2 in surveys_arrays_B:
+                try:
+                    ax.plot(
+                        ls,
+                        Dls_noise[f"{sv_ar1}x{sv_ar2}"][f] / fac,
+                        label=f"{sv_ar1}x{sv_ar2}",
+                    )
+                except:
+                    pass
+
+        ax.set_xlabel(r"$\ell$", fontsize=18)
+        ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
+        ax.set_title(f)
+        # ax.set_yscale('log')
+        ax.set_ylim(*plot_info["noise_BxB"]["ylims"][f])
+        ax.set_xlim(*plot_info["noise_BxB"]["xlims"][f])
+        ax.legend()
+        plt.savefig(save_path_cross_noises + f"corr_noise_{survey_B}x{survey_B}_{f}")
+        plt.close()
+
 # Define surveys and arrays to plot
 for survey_A in ['dr6', 'planck']:
+    if survey_A not in d['surveys']: continue
     arrays_A = d[f"arrays_{survey_A}"]
     # arrays_A = ['pa5_f090']
     surveys_arrays_A = [f"{survey_A}_{ar}" for ar in arrays_A]
@@ -85,22 +272,7 @@ for survey_A in ['dr6', 'planck']:
     surveys = [survey_A, survey_B]
     surveys_arrays = surveys_arrays_A + surveys_arrays_B
 
-    # Define where and what to plot
-
-    save_path = plot_dir + "/spectra/"
-    os.makedirs(save_path, exist_ok=True)
-    save_path_cross = save_path + "cross/"
-    os.makedirs(save_path_cross, exist_ok=True)
-    save_path_cross_freqs = save_path + "cross_freqs/"
-    os.makedirs(save_path_cross_freqs, exist_ok=True)
-    save_path_cross_noises = save_path + "noises/"
-    os.makedirs(save_path_cross_noises, exist_ok=True)
-    save_path_TF = save_path + "TF/"
-    os.makedirs(save_path_TF, exist_ok=True)
-
     # Load spectra
-    Dls_cross = {}
-    Dls_noise = {}
     for sv_ar1, sv_ar2 in itertools.combinations_with_replacement(surveys_arrays, r=2):
         ls, Dls_cross[f"{sv_ar1}x{sv_ar2}"] = so_spectra.read_ps(
             spectra_cross_template.format(sv_ar1, sv_ar2), spectra=spectra
@@ -113,24 +285,11 @@ for survey_A in ['dr6', 'planck']:
             pass
     fac = ls * (ls + 1) / (2 * np.pi)
 
-    clfile = "/global/cfs/cdirs/sobs/users/merrydup/deep56/proposal_plots/cmb.dat"
-    l, ps_theory = so_spectra.read_ps(clfile, spectra=spectra)
-    lmax = d["lmax"]
-    binning_file = d["binning_file"]
-    Dlb_theory_dumb = {
-        spec: pspy_utils.naive_binning(
-            l[:lmax],
-            ps_theory[spec][:lmax] * l[:lmax] * (l[:lmax] + 1) / (2 * np.pi),
-            binning_file=binning_file,
-            lmax=d["lmax"],
-        )[1]
-        for spec in ["TT", "EE"]
-    }
 
     # AxB cross plots
     for f in spectra:
         fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
-        ax.plot(l, ps_theory[f] * l * (l + 1) / (2 * np.pi), color="black", label="theory")
+        ax.plot(l, ps_theory[f], color="black", label="theory")
 
         for sv_ar1 in surveys_arrays_A:
             for sv_ar2 in surveys_arrays_B:
@@ -160,7 +319,7 @@ for survey_A in ['dr6', 'planck']:
                     fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
                     ax.plot(
                         l,
-                        ps_theory[f] * l * (l + 1) / (2 * np.pi),
+                        ps_theory[f],
                         color="black",
                         label="theory",
                     )
@@ -188,28 +347,7 @@ for survey_A in ['dr6', 'planck']:
                     )
                     plt.close()
 
-    # BxB cross plots
-    for f in spectra:
-        fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
-        ax.plot(l, ps_theory[f] * l * (l + 1) / (2 * np.pi), color="black", label="theory")
-
-        for sv_ar1, sv_ar2 in itertools.combinations_with_replacement(
-            surveys_arrays_B, r=2
-        ):
-            ax.plot(ls, Dls_cross[f"{sv_ar1}x{sv_ar2}"][f], label=f"{sv_ar1}x{sv_ar2}")
-
-        ax.set_xlabel(r"$\ell$", fontsize=18)
-        ax.set_ylabel(rf"$D^{{{f}}}_\ell$", fontsize=18)
-        ax.set_title(f)
-        ax.set_yscale(plot_info["yscale"][f])
-        ax.set_ylim(*plot_info["cross_AxB"]["ylims"][f])
-        ax.set_xlim(*plot_info["cross_AxB"]["xlims"][f])
-        ax.legend()
-
-        plt.savefig(save_path_cross + f"cross_{survey_B}x{survey_B}_{f}")
-        plt.close()
-
-    # BxB cross plots per frequency
+    # AxB cross plots per frequency
     frequencies = ["090", "150", "220", "280"]
     for freq1, freq2 in itertools.combinations_with_replacement(frequencies, r=2):
         if True in [freq1 in sv_ar for sv_ar in surveys_arrays]:
@@ -218,7 +356,7 @@ for survey_A in ['dr6', 'planck']:
                     fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
                     ax.plot(
                         l,
-                        ps_theory[f] * l * (l + 1) / (2 * np.pi),
+                        ps_theory[f],
                         color="black",
                         label="theory",
                     )
@@ -246,90 +384,6 @@ for survey_A in ['dr6', 'planck']:
                         + f"cross_{survey_B}x{survey_B}_{f}_{freq1}x{freq2}"
                     )
                     plt.close()
-
-    # BxB noise plots
-    for f in spectra_auto:
-        fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
-        ax.plot(l, ps_theory[f], color="black", label="theory")
-
-        for sv_ar2 in surveys_arrays_B:
-            ax.plot(ls, Dls_noise[f"{sv_ar2}x{sv_ar2}"][f] / fac, label=f"{sv_ar2}")
-
-        ax.set_xlabel(r"$\ell$", fontsize=18)
-        ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
-        ax.set_title(f)
-        ax.set_yscale("log")
-        ax.set_ylim(*plot_info["noise_BxB"]["ylims"][f])
-        ax.set_xlim(*plot_info["noise_BxB"]["xlims"][f])
-        ax.legend()
-        plt.savefig(save_path_cross_noises + f"noise_{survey_A}x{survey_B}_{f}")
-        plt.close()
-
-    # BxB correlated noise plots
-    for f in spectra:
-        if f != f[::-1]:
-            fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
-            ax.plot(l, ps_theory[f], color="black", label="theory")
-
-            for sv_ar1 in surveys_arrays_B:
-                for sv_ar2 in surveys_arrays_B:
-                    try:
-                        ax.plot(
-                            ls,
-                            Dls_noise[f"{sv_ar1}x{sv_ar2}"][f] / fac,
-                            label=f"{sv_ar1}x{sv_ar2}",
-                        )
-                    except:
-                        pass
-
-            ax.set_xlabel(r"$\ell$", fontsize=18)
-            ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
-            ax.set_title(f)
-            # ax.set_yscale('log')
-            ax.set_ylim(*plot_info["noise_BxB"]["ylims"][f])
-            ax.set_xlim(*plot_info["noise_BxB"]["xlims"][f])
-            ax.legend()
-            plt.savefig(save_path_cross_noises + f"corr_noise_{survey_A}x{survey_B}_{f}")
-            plt.close()
-
-
-    # BxB noise plots
-    Nls = {}
-    for rms in [5, 10, 15, 20, 25, 30]:
-        ls_nls, Nls[rms] = pspy_utils.get_nlth_dict(
-            rms, type="Cl", lmax=d["lmax"], spectra=spectra
-        )
-
-    # Choose a colormap and create a list of colors
-    cmap = plt.get_cmap("viridis")  # try 'plasma', 'coolwarm', 'turbo', etc.
-    norm = Normalize(vmin=0, vmax=5)
-    colors = [cmap(norm(i)) for i in range(6)]
-
-    for f in spectra_auto:
-        fig, ax = plt.subplots(dpi=150, figsize=(7, 4))
-        ax.plot(l, ps_theory[f], color="black", label="theory")
-        for sv_ar2 in surveys_arrays_B:
-            ax.plot(
-                ls,
-                Dls_noise[f"{sv_ar2}x{sv_ar2}"][f] / fac * beams[sv_ar2] ** 2,
-                label=f"{sv_ar2}",
-            )
-
-        for i, (rms, nls) in enumerate(Nls.items()):
-            ax.plot(
-                ls_nls, nls[f], label=f"{rms}", color=colors[i], lw=4, alpha=0.4, zorder=-10
-            )
-
-        ax.set_xlabel(r"$\ell$", fontsize=18)
-        ax.set_ylabel(rf"$N^{{{f}}}_\ell$", fontsize=18)
-        ax.set_title(f)
-        ax.set_yscale("log")
-        ax.set_ylim(*plot_info["noise_rms_BxB"]["ylims"][f])
-        ax.set_xlim(*plot_info["noise_rms_BxB"]["xlims"][f])
-        ax.legend()
-        plt.savefig(save_path_cross_noises + f"noise_rms_{survey_A}x{survey_B}_{f}")
-        plt.close()
-
 
     # Plot Transfer function
     xlims = {

3e57687c880cea6d8fc423aadb2ae4a19866c8fa
Louis Thibaut <louisthibaut@client-172-18-120-70.eduroam.universite-paris-saclay.fr>
2026-04-09 15:12:33 +0200

    plot 2dtf

---
+++ project/SPT_dev/spt_sim/spt_plot_tf2d.py
@@ -0,0 +1,106 @@
+import pylab as plt
+import numpy as np
+import healpy as hp
+import sys
+
+from pspy import pspy_utils, so_dict, so_mcm, so_spectra
+from pspipe_utils import log, pspipe_list
+import candl
+import spt_candl_data
+
+def show_alm_triangle(
+        alms, lmax, vmin, vmax, real=True, cmap="seismic",
+        xlims=None, ylims=None,
+        title="Triangle", fig_file=None):
+        
+    """
+    This routine is from the spt3g data release
+    https://pole.uchicago.edu/public/data/quan26/index.html
+    Parameters
+    ----------
+    alms: array
+      a set of alms, the shape of alms should correspond to so_map.ncomp
+    lmax: int
+      the maximum multipole of the transform
+    real: boolean
+      withtout to take the real part or not
+    vmin, vmax: float
+      range of the colorbar
+    cmap: string
+      name of the colormap
+    xlims, ylims: int
+      x is for the \ell coordiante, y for the m coordinate
+    title: str
+     title, if multiple alm, will appended an integer number 0,1,2...
+    """
+    
+
+    import warnings
+    warnings.filterwarnings("ignore")
+    
+    def triangle_plot(alm, lmax, vmin, vmax, real=real, cmap="seismic", xlims=xlims, ylims=ylims, title=title, fig_file=fig_file):
+    
+        triangle = np.empty((lmax+1, lmax+1))
+        triangle[:,:] = np.nan
+        for l in range(lmax+1):
+            for m in range(0, l+1):
+                i = hp.Alm.getidx(lmax, l, m)
+                if real:
+                    triangle[m, l] = alm[i].real
+                else:
+                    triangle[m, l] = alm[i]
+
+        plt.figure(figsize=(7, 7))
+        img = plt.imshow(
+            triangle, origin="lower", cmap=cmap, vmin=vmin, vmax=vmax)
+        if xlims is None:
+            xlims = [0, triangle.shape[1]]
+        if ylims is None:
+            ylims = [0, triangle.shape[0]]
+        plt.xlim(left=xlims[0], right=xlims[1])
+        plt.ylim(bottom=ylims[0], top=ylims[1])
+        plt.grid(False)
+        cb = plt.colorbar(pad=0.03, shrink=0.8)
+        plt.grid(True)
+        plt.xlabel(r"$\ell$")
+        plt.ylabel(r"$m$")
+        plt.title(title)
+        plt.savefig(fig_file)
+        plt.clf()
+        plt.close()
+
+    if alms.ndim != 1:
+        for i in range(len(alms)):
+            triangle_plot(alms[i], lmax, vmin, vmax, title= title + "_%d" % i)
+    else:
+        triangle_plot(alms, lmax, vmin, vmax, title=title)
+
+
+
+
+d = so_dict.so_dict()
+d.read_from_file(sys.argv[1])
+log = log.get_logger(**d)
+
+survey = "spt"
+tf_dir = "tf2d"
+arrays_spt = d["arrays_spt"]
+
+tf_dir = "tf2d"
+plot_dir = "plots/tf2d"
+
+pspy_utils.create_directory(plot_dir)
+
+
+vmax = {}
+vmax["TT"] = 6000
+vmax["EE"] = 60
+vmax["BB"] = 0.2
+
+for ar in arrays_spt:
+    for i, comp in enumerate(["TT", "EE", "BB"]):
+        for filt in [ "nofilter", "filter"]:
+            ps_2d = np.load(f"{tf_dir}/tf2d_{comp}_{ar}_{filt}.npy")
+
+            show_alm_triangle(ps_2d, d["lmax"], 0, vmax[comp], xlims=[0,2000], ylims=[0,2000], title=f"{ar} {comp} {filt}", fig_file = f"{plot_dir}/{ar}_{comp}_{filt}.png" )
+
